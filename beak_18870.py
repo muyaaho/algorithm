341781918fbfd5027e2472f2cc3f7d150fd3fa5d
muyaaho <skanthsu715@gmail.com>
2022-12-18 15:34:17 +0900

    commit

--- beak_18870.py
+++ beak_18870.py
@@ -1,11 +1,6 @@
 from sys import stdin
 
-a = int(stdin.readline())
-arr = list(map(int, stdin.readline().split()))
+a=int(input())
+b=list(map(int, input().split()))
 
-sarr = sorted(set(arr))
-dict=dict(enumerate(sarr))
-print(dict)
-
-for x in arr:
-    print(dict.get(x), end='')
+#작성하세요
