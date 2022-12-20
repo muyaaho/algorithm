6db668ea4ea0aeca370e9a7b264ae2949bbc220a
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-20 14:31:51 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -5,18 +5,11 @@ for x in range(n):
     l.append(int(input()))
 
 l = list(filter(lambda x : x<=k, l))
-p = l[-1]
-while k > 0:
-    for x in l[::-1]:
-        if k>=x:
-            if p >x and len(l)>1:
-                l.pop(-1)
-                p = l[-1]
-                #print(l)    
-            answer += k//x
-            k %= x
-            
-            #print(f'money: {k}, answer: {answer}')
-            break
+
+for x in l[::-1]:
+    answer += k//x
+    k%=x
+    if k<=0:
+        break
 
 print(answer)
