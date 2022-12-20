e20026448190c0e2302bd6a44ae5d4e0d6fc351d
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-20 14:58:21 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,14 @@
+n, m = map(int,input().split())
+ans = []
+
+def func():
+    if len(ans)==m:
+        print(*ans)
+        return
+    for x in range(1,n+1):
+        if x not in ans:
+            ans.append(x)
+            func()
+            ans.pop()
+
+func()
