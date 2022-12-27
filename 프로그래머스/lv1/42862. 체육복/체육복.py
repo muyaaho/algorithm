8486911e567530c990d472274f594ed2f1468e9a
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-27 21:09:43 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,17 +1,22 @@
 def solution(n, lost, reserve):
-
-    l = [1]*(n+1)
+    answer = 0
+    people = [1] * (n+1)
     
-    for x in lost: l[x] -= 1
-    for x in reserve: l[x] += 1
-    #print(f'set: {l}')
-    for i in range(n+1):
-        if l[i] == 2 and i > 1 and l[i-1] == 0:
-            l[i-1] += 1
-            l[i] -= 1
-        elif l[i] == 2 and i<n and l[i+1] == 0:
-            l[i+1] += 1
-            l[i] -= 1
-    #print(f'change: {l}')
-    answer = len([x for x in l if x != 0])-1
-    return answer
+    for x in lost:
+        people[x] -= 1
+    
+    for x in reserve:
+        people[x] += 1
+    
+    for x in range(n+1):
+        if x>1 and people[x] == 2 and people[x-1]==0:
+            people[x-1] += 1
+            people[x] -= 1
+        elif x<n and people[x] == 2 and people[x+1] == 0:
+            people[x+1] += 1
+            people[x] -= 1
+    
+    for x in people:
+        if x != 0:
+            answer += 1
+    return answer -1
