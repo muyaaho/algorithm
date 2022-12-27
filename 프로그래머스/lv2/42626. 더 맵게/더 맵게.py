544d43070f66b2f410015be908f6e469098cbe37
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-27 15:03:08 +0900

    [level 2] Title: 더 맵게, Time: 1730.56 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -2,15 +2,13 @@ from heapq import heappop, heapify, heappush
 
 def solution(scoville, K):
     answer = 0
-    
     heapify(scoville)
+    
     while len(scoville) > 1:
         if scoville[0]<K:
             heappush(scoville, heappop(scoville) + heappop(scoville)*2)
-            answer += 1        
+            answer += 1
         else:
             break
-    
-    if len(scoville) == 1 and scoville[0] < K : answer = -1
-        
+    if len(scoville) == 1 and scoville[0] < K: answer = -1
     return answer
