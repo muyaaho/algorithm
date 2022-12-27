79e55dbd3f77ae7af3464a9f971e23228b30deec
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-27 15:37:04 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = set()
+    d = 2
+    
+    while d<=n:
+        if n%d == 0:
+            answer.add(d)
+            n //= d
+        else:
+            d += 1
+            
+    
+    return sorted(list(answer))
