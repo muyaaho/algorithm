7af5b05819c264003a1511a1f638f6d81125728d
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-30 08:45:10 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,18 +1,15 @@
 def solution(n, lost, reserve):
-    arr = [1]*(n+1)
+    p = [1]*(n+1)
     
-    for x in lost:
-        arr[x] -= 1
-    for x in reserve:
-        arr[x] += 1
+    for x in lost: p[x] -= 1
+    for x in reserve: p[x] += 1
     
-    for x in range(n+1):
-        if x > 1 and arr[x] == 2 and arr[x-1] == 0:
-            arr[x-1] += 1
-            arr[x] -= 1
-        elif x < n and arr[x] == 2 and arr[x+1] == 0:
-            arr[x+1] += 1
-            arr[x] -= 1
-    
-    answer = [x for x in arr if x != 0]
-    return len(answer)-1
+    for i in range(n+1):
+        if i>1 and p[i] == 2 and p[i-1] == 0:
+            p[i] -= 1
+            p[i-1] += 1
+        elif i<n and p[i] == 2 and p[i+1] == 0:
+            p[i] -= 1
+            p[i+1] += 1
+            
+    return n-len([x for x in p if x == 0])
