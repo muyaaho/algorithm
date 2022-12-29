713c383d2870308d15597749d4b5ce40fdce964b
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-29 14:51:36 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,22 +1,18 @@
 def solution(n, lost, reserve):
-    answer = 0
-    people = [1] * (n+1)
+    arr = [1]*(n+1)
     
     for x in lost:
-        people[x] -= 1
-    
+        arr[x] -= 1
     for x in reserve:
-        people[x] += 1
+        arr[x] += 1
     
     for x in range(n+1):
-        if x>1 and people[x] == 2 and people[x-1]==0:
-            people[x-1] += 1
-            people[x] -= 1
-        elif x<n and people[x] == 2 and people[x+1] == 0:
-            people[x+1] += 1
-            people[x] -= 1
+        if x > 1 and arr[x] == 2 and arr[x-1] == 0:
+            arr[x-1] += 1
+            arr[x] -= 1
+        elif x < n and arr[x] == 2 and arr[x+1] == 0:
+            arr[x+1] += 1
+            arr[x] -= 1
     
-    for x in people:
-        if x != 0:
-            answer += 1
-    return answer -1
+    answer = [x for x in arr if x != 0]
+    return len(answer)-1
