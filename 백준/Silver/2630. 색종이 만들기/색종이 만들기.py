7e02591c14690abdba258b28a1cd8aa772c28295
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-30 13:49:44 +0900

    [Silver II] Title: 색종이 만들기, Time: 60 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,34 +1,33 @@
 n = int(input())
-arr = []
-for x in range(n):
-    arr.append(list(map(int, input().split())))
-b,w = 0,0
+arr = [list(map(int, input().split())) for x in range(n)]
+blue, white = 0, 0
 
-def check(a):
-    global b,w
+
+def func(ttarr):
+    # 재귀 탈출
+    # 다 1이면 count 추가 아니면 재귀 계속
+    global blue, white
+    cnt1, cnt0 = 0,0
+    size = len(ttarr)
+    for x in ttarr:
+        if 1 not in x:
+            cnt0 += 1
+        elif 0 not in x:
+            cnt1 += 1
     
-    cntb,cntw=0,0
-    for x in a:
-        # print(x)
-        if 0 not in x:
-            cntb += 1
-        elif 1 not in x:
-            cntw += 1
-
-    if cntb == len(a):
-        b += 1
+    if cnt0 == size:
+        white += 1
         return
-    elif cntw == len(a):
-        w += 1
+    elif cnt1 == size:
+        blue += 1
         return
+    
+    
+    func([row[:size//2] for row in ttarr[:size//2]])
+    func([row[:size//2] for row in ttarr[size//2:]])
+    func([row[size//2:] for row in ttarr[:size//2]])
+    func([row[size//2:] for row in ttarr[size//2:]])
 
-    div = len(a)
-    
-    check([row[0:div//2] for row in a[0:div//2]])
-    check([row[0:div//2] for row in a[div//2:div]])
-    check([row[div//2:div] for row in a[0:div//2]])
-    check([row[div//2:div] for row in a[div//2:div]])
-    
-check(arr)
-print(w)
-print(b)
+func(arr)
+print(white)
+print(blue)
