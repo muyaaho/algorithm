7f758ceb59da737a7d2ec509588baa6f80e7c6fd
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-30 10:10:20 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,4 +1,2 @@
-from collections import Counter
-
 def solution(order):
-    return Counter(str(order))["3"] + Counter(str(order))["6"] + Counter(str(order))["9"]
+    return sum(map(lambda x: str(order).count(str(x)), [3,6,9]))
