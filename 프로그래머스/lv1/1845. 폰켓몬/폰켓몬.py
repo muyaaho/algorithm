01c8887f00ade219dce5babd19527caddc7d6a80
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-30 16:46:42 +0900

    [level 1] Title: 폰켓몬, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,2 @@
 def solution(nums):
-    answer = 0
-    divnum = len(nums)//2
-    snums = set(nums)
-    
-    if divnum > len(snums):
-        answer = len(snums)
-    else:
-        answer = divnum
-    return answer
+    return min(len(nums)//2, len(set(nums)))
