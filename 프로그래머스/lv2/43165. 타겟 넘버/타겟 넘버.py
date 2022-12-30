b9322a7cecd19e7cc00fe837d0f0389b28a0bf21
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-30 14:12:03 +0900

    [level 2] Title: 타겟 넘버, Time: 3.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,15 @@
-from collections import Counter
-
 def solution(numbers, target):
     answer = []
     
-    def func(arr, x):
-        # arr가 없으면 리스트에 값 append 하기
-        if not arr:
-            answer.append(x)
-            return
-        # 아니면 더하거나 빼서 함수 진행
-        func(arr[1:], x+arr[0])
-        func(arr[1:], x-arr[0])
+    def cal(arr, start):
+        if not arr: 
+            answer.append(start)
+            return 
+        
+        cal(arr[1:], start+arr[0])
+        cal(arr[1:], start-arr[0])
     
-    func(numbers, 0)
-    return Counter(answer)[target]
+    cal(numbers[1:], +numbers[0])
+    cal(numbers[1:], -numbers[0])
+    
+    return answer.count(target)
