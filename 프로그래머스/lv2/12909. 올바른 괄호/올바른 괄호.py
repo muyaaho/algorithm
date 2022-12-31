24cc001bfe6a20b09383617306c2f1e3ce181fd9
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-31 21:24:44 +0900

    [level 2] Title: 올바른 괄호, Time: 5.96 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -2,24 +2,18 @@ from collections import deque
 
 def solution(s):
     answer = True
-    queue = deque()
-    queue.append(s[0])
-    for x in s[1:]:
-        if not queue and x == ")":
-            return False
-        elif x == ")" and queue[-1] == "(":
-            queue.pop()
-        else:
-            queue.append(x)
-        # print(f'x: {x}, queue: {queue}')
     
-    if not queue:
+    q = deque()
+    for x in s:
+        if x == '(':
+            q.append(x)
+        else:
+            try:
+                q.pop()
+            except:
+                return False
+    
+    if not q:
         return True
     else:
         return False
-    
-# print(solution("()()"))
-# print(solution("(())()"))
-# print(solution(")()("))
-# print(solution("(()("))
-# print(solution("()())(()"))
