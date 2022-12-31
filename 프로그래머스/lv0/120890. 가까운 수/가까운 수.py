8b57897c65dbed7c3d102971756f4d68311da73c
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-31 22:47:27 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,8 +1,2 @@
 def solution(array, n):
-    array.sort(key=lambda x: (abs(x-n), x-n))
-    return array[0]
-
-# def solution(array, n):
-#     array.sort(key = lambda x : (abs(x-n), x-n))
-#     answer = array[0]
-#     return answer
+    return sorted(array, key=lambda x: (abs(x-n), x-n))[0]
