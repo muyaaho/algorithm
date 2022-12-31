c0b65052509adb05a6cc0db45f25fada10231102
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-31 15:06:48 +0900

    [level 2] Title: 타겟 넘버, Time: 5.38 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,12 @@
 def solution(numbers, target):
     answer = []
     
-    def cal(arr, start):
-        if not arr: 
-            answer.append(start)
-            return 
-        
-        cal(arr[1:], start+arr[0])
-        cal(arr[1:], start-arr[0])
+    def func(arr, start):
+        if not arr:
+            if start == target: answer.append(1)
+            return
+        func(arr[1:], start+arr[0])
+        func(arr[1:], start-arr[0])
     
-    cal(numbers[1:], +numbers[0])
-    cal(numbers[1:], -numbers[0])
-    
-    return answer.count(target)
+    func(numbers, 0)
+    return len(answer)
