aca73d5c740eab527fac5ee26bb77d3e155a16c4
muyaaho <76798969+muyaaho@users.noreply.github.com>
2022-12-31 21:58:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 316 ms, Memory: 39568 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,13 +1,21 @@
 from collections import deque
 
 n, m, k = map(int, input().split())
-graph = [[0] * (n+1) for _ in range(n+1)]
+graph = [[0]*(n+1) for _ in range(n+1)]
+
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
+    graph[a][b] = graph[b][a] =1
 
-vbfs = [0]*(n+1)
 vdfs = [0]*(n+1)
+vbfs = [0]*(n+1)
+
+def dfs(now):
+    vdfs[now] = 1
+    print(now, end=' ')
+    for end in range(1, n+1):
+        if vdfs[end] == 0 and graph[now][end] == 1:
+            dfs(end)
 
 def bfs(now):
     q = deque()
@@ -16,19 +24,12 @@ def bfs(now):
 
     while q:
         now = q.popleft()
-        print(now, end=' ')
+        print(now, end = ' ')
         for end in range(1, n+1):
-            if vbfs[end] == 0 and graph[now][end] == 1:
+            if vbfs[end] == 0 and graph[now][end]==1:
                 q.append(end)
                 vbfs[end] = 1
 
-def dfs(now):
-    vdfs[now] = 1
-    print(now, end=' ')
-    for end in range(1, n+1):
-        if vdfs[end] == 0 and graph[now][end] == 1:
-            dfs(end)
 dfs(k)
 print()
 bfs(k)
-
