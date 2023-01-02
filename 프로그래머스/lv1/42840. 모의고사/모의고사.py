0c3574e1d1d5780607aff3036f9b08e716700c6c
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-02 22:30:27 +0900

    [level 1] Title: 모의고사, Time: 4.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,20 +1,20 @@
 def solution(answers):
     answer = []
     p1 = [1,2,3,4,5]
-    p2 = [2,1,2,3,2,4,2,5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
     p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    count = [0,0,0]
+    c = [0, 0, 0]
     
+    for i, x in enumerate(answers):
+        if p1[i%5] == x:
+            c[0] += 1
+        if p2[i%8] == x:
+            c[1] += 1
+        if p3[i%10] == x:
+            c[2] += 1
     
-    for i, score in enumerate(answers):
-        if p1[i%5] == score:
-            count[0] += 1
-        if p2[i%8] == score:
-            count[1] += 1
-        if p3[i%10] == score:
-            count[2] += 1
+    for i, x in enumerate(c, start=1):
+        if x == max(c):
+            answer.append(i)
     
-    for i in range(3):
-        if count[i] == max(count):
-            answer.append(i+1)
     return answer
