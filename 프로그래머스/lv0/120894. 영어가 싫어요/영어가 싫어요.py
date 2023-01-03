abd34279075743cb72998c8baa5bf6c35e2ae9e1
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-03 17:51:27 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,6 +1,4 @@
 def solution(numbers):
-    dict = {"zero":'0',"one":"1", "two":"2", "three":'3', "four":'4', "five": '5', "six": '6', "seven":'7', "eight":'8', "nine":'9'}
-    
-    for num in dict:
-        numbers = numbers.replace(num, dict[num])
+    for num, eng in enumerate([ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(num))
     return int(numbers)
