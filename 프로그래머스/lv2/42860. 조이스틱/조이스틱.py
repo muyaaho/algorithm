05216c359967cb8136a42b2504cb917c6e012a71
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-03 14:39:08 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -1,15 +1,10 @@
 def solution(name):
-    answer = 0
-    minm = len(name)-1
-    # 전체 만들 수 있는 수
-    for x in name:
-        answer += min(abs(ord('A')-ord(x)), 26-abs(ord('A')-ord(x)))
+    answer = sum(min(abs(ord('A') - ord(n)), 26-abs(ord('A')-ord(n))) for n in name )
+    mm = len(name)-1
     
-    # A가 연속으로 나올 때
     for i, c in enumerate(name):
-        next = i+1
-        while next < len(name) and name[next] == 'A':
-            next += 1
-        minm = min(minm, 2*i + len(name) - next, i+2*(len(name)-next))
-        
-    return answer+minm
+        nxt = i+1
+        while nxt < len(name) and name[nxt] == 'A':
+            nxt += 1
+        mm = min(mm, 2*i+len(name)-nxt, i+ 2*(len(name)-nxt))
+    return answer+mm
