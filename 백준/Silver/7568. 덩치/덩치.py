b9c739d24b5e32e009af3064e8ff7ebeb377376a
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-05 17:45:32 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,13 +1,9 @@
 n = int(input())
 peoples = [list(map(int, input().split())) for _ in range(n)]
-ans = []
 
-for now in peoples:
+for i in peoples:
     rank = 1
-    for before in peoples:
-        if now[0] < before[0] and now[1] < before[1]:
+    for j in peoples:
+        if i[0]<j[0] and i[1]<j[1]:
             rank += 1
-    ans.append(rank)
-
-print(*ans)
-
+    print(rank, end=' ')
