9670893550dd7434c6898ef54c1fc440993c8d4d
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-05 14:47:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,4 +1,4 @@
 def solution(my_string, num1, num2):
-    l = list(my_string)
-    l[num1], l[num2] = l[num2], l[num1]
-    return ''.join(l)
+    a = list(my_string)
+    a[num1], a[num2] = a[num2], a[num1]
+    return ''.join(a)
