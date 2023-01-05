377bdbe3cd4a599ae234c2afa63d988f1c6843e5
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-05 10:15:00 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,18 +1,18 @@
 def solution(N, number):
     answer = 0
     s = [set() for _ in range(8)]
-    for i, x in enumerate(s, start = 1):
-        x.add(int(str(N)*i))
-        
+    for i in range(8):
+        s[i].add(int(str(N)*(i+1)))
+    
     for i in range(8):
         for j in range(i):
-            for o1 in s[j]:
-                for o2 in s[i-j-1]:
-                    s[i].add(o1+o2)
-                    s[i].add(o1-o2)
-                    s[i].add(o1*o2)
-                    if o2 != 0:
-                        s[i].add(o1//o2)
+            for op1 in s[j]:
+                for op2 in s[i-j-1]:
+                    s[i].add(op1+op2)
+                    s[i].add(op1-op2)
+                    s[i].add(op1*op2)
+                    if op2 != 0:
+                        s[i].add(op1//op2)
         if number in s[i]:
             answer = i+1
             break
