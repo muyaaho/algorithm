2c1e12d2c45a803003a3822f514ff2988eb2434e
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-19 10:47:46 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,9 +1,8 @@
 n = int(input())
-
-coord = [list(map(int, input().split())) for _ in range(n)]
 area = [[0 for _ in range(101)] for _ in range(101)]
 
-for x, y in coord:
+for _ in range(n):
+    x, y = map(int, input().split())
     for i in range(x, x+10):
         for j in range(y, y+10):
             area[i][j] = 1
