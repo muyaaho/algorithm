24fbfc2d0c81cceef1fc3ff9f67cc40e66d30ed5
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-20 20:05:28 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,15 +1,26 @@
 def solution(n, lost, reserve):
-    p = [1]*(n+1)
-    
-    for x in lost: p[x] -= 1
-    for x in reserve: p[x] += 1
-    
-    for i in range(n+1):
-        if i>1 and p[i] == 2 and p[i-1] == 0:
-            p[i] -= 1
-            p[i-1] += 1
-        elif i<n and p[i] == 2 and p[i+1] == 0:
-            p[i] -= 1
-            p[i+1] += 1
-            
-    return n-len([x for x in p if x == 0])
+    answer = 0
+    k = 0
+    # 중복데이터 제거 및 카운팅
+    cp_lost = lost[:]
+    cp_reserve =reserve[:]
+
+    for i in range(len(lost)):
+        if lost[i] in reserve:            
+            cp_reserve.remove(lost[i])
+            cp_lost.remove(lost[i])
+    cp_lost.sort()
+    cp_reserve.sort()
+    while cp_lost and cp_reserve:
+        v = cp_lost.pop(0)
+        if v-1 in cp_reserve:
+            cp_reserve.remove(v-1)
+        elif v+1 in cp_reserve:
+            cp_reserve.remove(v+1)
+        else:
+            k += 1
+
+    if cp_lost:
+        k = k + len(cp_lost)
+    answer = n - k 
+    return answer
