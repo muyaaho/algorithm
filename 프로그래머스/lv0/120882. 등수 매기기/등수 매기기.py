38a93870fab1008b2fbed18ac6eb6cd7b6a3e359
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-20 10:01:14 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -1,8 +1,3 @@
-from collections import Counter
-
 def solution(score):
-    avg = [sum(a)/2 for a in score]
-    dict = {num:rank for rank, num in enumerate(sorted(avg, reverse=True), start = 1)}
-    c = Counter(avg)
-    
-    return [dict[a] - (c[a]-1) for a in avg]
+    a = sorted([sum(i) for i in score], reverse=True)
+    return [a.index(sum(i))+1 for i in score]
