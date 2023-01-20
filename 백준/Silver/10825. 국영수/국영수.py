5a68ab4571cf0c37ebf1f1bb6a0c5897d676fa06
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-20 15:08:17 +0900

    [Silver IV] Title: 국영수, Time: 4356 ms, Memory: 79928 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,15 +1,7 @@
-from operator import itemgetter
-
 n = int(input())
-arr = []
-for _ in range(n):
-    name, kor, eng, math = input().split()
-    arr.append((name, name.lower(), int(kor), int(eng), int(math)))
 
-d = sorted(arr, key=itemgetter(0))
-c = sorted(d, key=itemgetter(4), reverse=True)
-b = sorted(c, key=itemgetter(3))
-a = sorted(b, key=itemgetter(2), reverse=True)
+arr = [list(input().split()) for _ in range(n)]
 
-for x in a:
+arr.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+for x in arr:
     print(x[0])
