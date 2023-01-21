89173747a67551843ae8487ca7e937e44f203c6e
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-21 13:44:12 +0900

    [Silver IV] Title: 국영수, Time: 400 ms, Memory: 80696 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,7 +1,8 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
+n = int(input())
 arr = [list(input().split()) for _ in range(n)]
 
 arr.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
-for x in arr:
-    print(x[0])
+print(*[x[0] for x in arr], sep='\n')
