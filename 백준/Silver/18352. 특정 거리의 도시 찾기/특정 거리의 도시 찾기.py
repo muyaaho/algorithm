b096b6eebd17a30e046d21be6f6267d5baaaa90b
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-21 13:35:14 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1504 ms, Memory: 99628 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,28 +1,34 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n, m, k, x = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+graph = [[] for i in range(n+1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
 q = deque()
-v = [-1]*(n+1)
 q.append(x)
+v = [-1]*(n+1)
 v[x] = 0
+
 while q:
     now = q.popleft()
     if v[now] == k:
         break
-    for to_go in graph[now]:
-        if v[to_go] == -1:
-            v[to_go] = v[now]+1
-            q.append(to_go)
+    for to in graph[now]:
+        if v[to] == -1:
+            v[to] = v[now] + 1
+            q.append(to)
 
-if k in v:
-    for i, x in enumerate(v):
-        if x == k:
-            print(i)
-else:
+c = False
+for i, x in enumerate(v):
+    if x == k:
+        c = True
+        print(i)
+    
+
+if not c:
     print(-1)
