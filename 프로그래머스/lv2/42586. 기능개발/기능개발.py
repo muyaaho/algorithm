265a8e63815ad4057898801792f8553f884d3efb
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-23 17:22:45 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,25 +1,8 @@
 def solution(progresses, speeds):
-    answer = []
-
-    left = [0]*(len(progresses))
-    for i in range(len(progresses)):
-        p = progresses[i]
-        s = speeds[i]
-        if (a:=(100-p)/s)%1 > 0:
-            left[i] += 1
-        left[i] += (100-p)//s
-
-    stack = []
-    for x in left:
-        if not stack:
-            stack.append(x)
-        elif stack[0] >= x:
-            stack.append(x)
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
         else:
-            answer.append(len(stack))
-            stack.clear()
-            stack.append(x)
-    if stack:
-        answer.append(len(stack))
-            
-    return answer
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
