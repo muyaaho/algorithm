3152d6dd478d484d419cd9c9d66a62ee4703d062
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-24 13:39:22 +0900

    [Silver III] Title: 통계학, Time: 384 ms, Memory: 53108 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,19 +1,19 @@
-from collections import Counter
 import sys
 input = sys.stdin.readline
+from collections import Counter
 
 n = int(input())
 arr = [int(input()) for _ in range(n)]
-
 arr.sort()
+# 평균
+print(round(sum(arr)/n))
 
-print(round(sum(arr)/n))    # 산술
-print(arr[(n-1)//2])        # 중앙값
+# 중앙값
+print(arr[(n-1)//2])
 
-c = Counter(arr).most_common()  # 최빈값
-if len(arr)>1 and c[0][1] == c[1][1]:
-    print(c[1][0])
-else:
-    print(c[0][0])
+# 최빈값
+c = Counter(arr).most_common(2)
+print(c[1][0] if len(arr)>1 and c[0][1] == c[1][1] else c[0][0])
 
-print(arr[-1]- arr[0])      # 범위
+# 범위
+print(arr[-1] - arr[0])
