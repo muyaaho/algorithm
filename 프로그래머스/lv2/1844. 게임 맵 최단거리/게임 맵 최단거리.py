c92d2a05e024bf148313c7345c8e83060dfb73f2
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-25 11:08:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,23 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
+    answer = 0
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
     
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, 1, -1]
-    
+    # 1일 때는 그냥 값 넣고 1이 아니라면 최솟값 집어넣기
     q = deque()
-    q.append((0, 0))
-    v = [[False]*m for i in range(n)]
-    v[0][0] = True
-    
+    q.append((0,0))
     while q:
         x, y = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx <n and 0<= ny < m and not v[nx][ny] and maps[nx][ny] == 1:
-                q.append((nx, ny))
-                v[nx][ny] = True
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] == 1:
                 maps[nx][ny] += maps[x][y]
-    
-    return maps[-1][-1] if maps[-1][-1]> 1 else -1
+                q.append((nx, ny))
+        # for x in maps:
+        #     print(x)
+        # print('--------------------')
+
+    return a if (a:=maps[-1][-1]) > 1 else -1
