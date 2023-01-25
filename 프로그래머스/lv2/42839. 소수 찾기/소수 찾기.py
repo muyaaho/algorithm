3cce91ff53bed3775db3c3599dd2d58be04cfb13
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-25 11:48:31 +0900

    [level 2] Title: 소수 찾기, Time: 0.12 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,7 +1,7 @@
 from itertools import permutations
 
 n = 10000000
-a = [False, False] + [True]*(n-1)
+a = [False, False] + [True] *(n-1)
 primes = set()
 
 for i in range(2, n+1):
@@ -9,14 +9,15 @@ for i in range(2, n+1):
         primes.add(i)
         for j in range(2*i, n+1, i):
             a[j] = False
+            
 
 def solution(numbers):
     answer = set()
     
     for i in range(1, len(numbers)+1):
         for x in permutations(numbers, i):
-            s = int(''.join(x))
-            if (s in primes):
-                answer.add(s)
-        
+            x = int(''.join(x))
+            if x in primes:
+                answer.add(x)
+    
     return len(answer)
