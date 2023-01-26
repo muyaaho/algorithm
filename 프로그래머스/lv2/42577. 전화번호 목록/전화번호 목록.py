dca3b96a6d62326b4cf13c8db71f0f8f57e7f1f1
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-26 10:02:47 +0900

    [level 2] Title: 전화번호 목록, Time: 85.41 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,7 @@
 def solution(phone_book):
     phone_book.sort()
     
-    for i, x in enumerate(phone_book[:-1]):
-        if phone_book[i+1].find(x) == 0:
+    for i, x in enumerate(phone_book[:-1], start = 1):
+        if phone_book[i].find(x) == 0:
             return False
-    
     return True
