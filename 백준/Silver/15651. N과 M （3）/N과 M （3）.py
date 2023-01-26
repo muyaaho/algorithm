f6ff9ef222cab7bcfc1f9a645975eff27d39f740
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-01-26 10:11:40 +0900

    [Silver III] Title: N과 M (3), Time: 1964 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
 
 arr = []
@@ -8,10 +5,8 @@ def f():
     if len(arr) == m:
         print(*arr)
         return
-    
     for i in range(1, n+1):
         arr.append(i)
         f()
         arr.pop()
-
 f()
