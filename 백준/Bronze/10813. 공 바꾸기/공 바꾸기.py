651e3e16e852838a391b8c4517e74b91266d0377
muyaaho <76798969+muyaaho@users.noreply.github.com>
2023-02-23 20:53:46 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(range(n+1))
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    arr[i], arr[j] = arr[j], arr[i]
+
+print(*arr[1:])
