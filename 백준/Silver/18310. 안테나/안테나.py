1128a359f7c1a512bd90ecc2e59f435bb37f1ba8
서지희_T5109 <76798969+muyaaho@users.noreply.github.com>
2023-03-07 22:03:37 +0900

    [Silver III] Title: 안테나, Time: 132 ms, Memory: 52128 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.py
+++ 백준/Silver/18310. 안테나/안테나.py
@@ -2,7 +2,6 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-houses = list(map(int, input().split()))
-
-houses.sort()
-print(houses[(n-1)//2])
+arr = list(map(int, input().split()))
+arr.sort()
+print(arr[(n-1)//2])
