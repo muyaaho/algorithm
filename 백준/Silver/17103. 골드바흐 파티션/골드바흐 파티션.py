885acdc15401ea3598872ff8cc58ebde40cdfbd0
서지희_T5109 <76798969+muyaaho@users.noreply.github.com>
2023-03-29 22:04:13 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 1088 ms, Memory: 46876 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,27 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-# t = int(input())
+t = int(input())
+arr = [int(input()) for _ in range(t)]
 
 # 에라토스테네스의 체
-m = 1000000
+m = max(arr)
 a = [False, False] + [True] *(m-1)
-# primes = []
+primes = []
 
 
 for i in range(2, m+1):
     if a[i]:
-        # primes.append(i)
+        primes.append(i)
         for j in range(2*i, m+1, i):
             a[j] = False
 
-for _ in range(int(input())):
-    n = int(input())
-    # 에스토스테네스의 체까지 가는 방법? 작으면 break 하기?
 
+for n in arr:
     ans = 0
-    for i in range((n//2)+1):
-        if a[i] and a[n-i]:
+    for i in primes:
+        if i>=n:
+            break
+        if a[n-i] and i <= n-i:
             ans += 1
     
     print(ans)
+
