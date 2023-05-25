2c528d2a2b999bdca9eebffe29ddd1cfcaa70420
서지희_T5109 <76798969+muyaaho@users.noreply.github.com>
2023-05-26 08:19:14 +0900

    [Silver IV] Title: 제로, Time: 152 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,11 +1,16 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 q = deque()
+ans = 0
 for _ in range(int(input())):
     a = int(input())
     if a != 0:
         q.append(a)
+        ans += a
     else:
-        q.pop()
+        p = q.pop()
+        ans -= p
 
-print(sum(q))
+print(ans)
