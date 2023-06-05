8fd696dfc2a679fbbcdf5c3d85dada4ec0690357
서지희_T5109 <76798969+muyaaho@users.noreply.github.com>
2023-06-05 09:13:49 +0900

    [Silver II] Title: 222-풀링, Time: 564 ms, Memory: 75836 KB -BaekjoonHub

--- 백준/Silver/17829. 222－풀링/222－풀링.py
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -1,29 +1,24 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
+n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 def f(arr):
     if len(arr)==1:
-        print(arr[0][0])
-        # print('여기서 리턴함여:', arr[0][0])
-        return None
-    else:
-        ans = []
-        for x in range(0, len(arr)//2):
-            ans.append([])
-            for y in range(0, len(arr)//2):
-                x2 = x*2
-                y2 = y*2
-                l1 = []
-                # print(x, y)
-                l1.extend(arr[x2][y2:y2+2])
-                l1.extend(arr[x2+1][y2:y2+2])
-                # print(l1)
-                l1.sort()
-                # print(l1)
-                # print(l1[2])
-                ans[x].append(l1[2])
-            # print(ans)
-        f(ans)
+        return arr[0][0]
+    ans = []
+    for x in range(0, len(arr)//2):
+        ans.append([])
+        for y in range(0, len(arr)//2):
+            x2 = x*2
+            y2 = y*2
+            
+            l1 = []
+            l1.extend(arr[x2][y2:y2+2])
+            l1.extend(arr[x2+1][y2:y2+2])
 
-f(arr)
+            l1.sort()
+            ans[x].append(l1[2])
+    return f(ans)
+print(f(arr))
