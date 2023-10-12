6a4353254fb830ad7c8d53191a87cdb297d63e9b
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-10-12 11:50:00 +0900

    [Gold V] Title: AC, Time: 160 ms, Memory: 45176 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,35 +1,38 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 def list_print(arr, is_reverse):
     if not arr:
         print('[]')
     else:
         if is_reverse:
-            arr = list(reversed(arr))
+            arr.reverse()
         print('[' + ','.join(arr)+']')
 
 for _ in range(int(input())):
     cmds = input()
     n = int(input())
     arr = input().rstrip().strip('[]').split(',')
-    if '' in arr:
+    if n == 0:
         arr = []
     
     is_error = False
     is_reverse = False
-    
+    q = deque(arr)
     for cmd in cmds:
         if cmd == 'R':
             is_reverse = not is_reverse
-        elif cmd == 'D':    
-            idx = -1 if is_reverse else 0
-            try:
-                arr.pop(idx)
-            except:
+        elif cmd == 'D':
+            if not q:
                 print('error')
                 is_error = True
                 break
-    
+            else:
+                if is_reverse:
+                    q.pop()
+                else:
+                    q.popleft()
+
     if not is_error:
-        list_print(arr, is_reverse)  
+        list_print(q, is_reverse)  
