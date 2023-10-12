4f9a19dabd009a59d331cbe1a22df747103af4e0
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-10-12 10:39:10 +0900

    [Silver V] Title: 집합, Time: 3648 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,26 +1,29 @@
+'''
+set - Pypy3로 겨우 됨
+dp처럼 인덱스로 값을 접근하면 훨씬 빠르게 가능
+
+'''
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = set()
+arr = [0]*21
 
+# print(0 == False)
 for _ in range(n):
     cmd = input().split()
-    if cmd[0] == 'add':
-        arr = arr|{cmd[1]}
-    elif cmd[0] == 'remove':
-        arr = arr - {cmd[1]}
-    elif cmd[0] == 'check':
-        if not arr or cmd[1] not in arr:
-            print(0)
-        else:
-            print(1)
-    elif cmd[0] == 'toggle':
-        if len(arr) == 0 or cmd[1] not in arr:
-            arr.add(cmd[1])
-        else:
-            arr.remove(cmd[1])
-    elif cmd[0] == 'all':
-        arr = set(map(str,range(1,21)))
+    if cmd[0] == 'all':
+        arr = [1] * 21
     elif cmd[0] == 'empty':
-        arr = set()
+        arr = [0] * 21
+    else:
+        x = int(cmd[1])
+        if cmd[0] == 'add':
+            arr[x] = 1
+        elif cmd[0] == 'remove':
+            arr[x] = 0
+        elif cmd[0] == 'check':
+            print(1 if arr[x] else 0)
+        elif cmd[0] == 'toggle':
+            arr[x] = 0 if arr[x] else 1
