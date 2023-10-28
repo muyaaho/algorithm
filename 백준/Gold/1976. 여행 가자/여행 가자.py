085f3f365127f770db87a892e3e95a14dd1f4eb8
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-10-28 14:19:27 +0900

    [Gold IV] Title: 여행 가자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -11,28 +14,22 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
+
 n = int(input())
 m = int(input())
-parent = [i for i in range(n+1)]
 
-# union 연산을 각각 수행
+parents = [i for i in range(n+1)]
 for i in range(n):
-    data = list(map(int, input().split()))
-    for j in range(n):
-        if data[j] == 1:    # 연결된 경우 union 연산 수행
-            union_parent(parent, i+1, j+1)
+    line = list(map(int, input().split()))
+    for j, x in enumerate(line):
+        if x == 1:
+            union_parent(parents, i+1, j+1)
 
-# 여행 계획 입력 받기
-plan = list(map(int, input().split()))
+plans = list(map(int, input().split()))
 
 result = True
-# 여행 계획에 속하는 모든 노드의 루트가 동일한지 확인
 for i in range(m-1):
-    if find_parent(parent, plan[i]) != find_parent(parent, plan[i+1]):
+    if find_parent(parents, plans[i]) != find_parent(parents, plans[i+1]):
         result = False
-
-# 여행 계획에 속하는 모든 노드가 서로 연결되어있는지(루트가 동일한지) 확인
-if result:
-    print('YES')
-else:
-    print('NO')
+        
+print("YES" if result else "NO")
