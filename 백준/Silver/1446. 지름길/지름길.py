55d2bd616728dfcdc6f2c00ef5ff60dff9ff25b4
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-01 15:02:28 +0900

    [Silver I] Title: 지름길, Time: 68 ms, Memory: 36272 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,30 +1,30 @@
-import sys
-input = sys.stdin.readline
+from heapq import heappush, heappop
 from collections import deque
 
 n, d = map(int, input().split())
 graph = [[] for _ in range(d+1)]
-distance = [int(1e9)] * (d+1)
 
 for i in range(d):
     graph[i].append((i+1, 1))
 
 for _ in range(n):
-    start, end, cost = map(int, input().split())
-    
+    start, end, cost= map(int, input().split())
     if end > d:
         continue
     graph[start].append((end, cost))
-    
-q = deque()
-q.append((0, 0))
+
+q =[]
+# 큐에 뭐가 들어가는지 기억하자 .. ㅠㅠ 그냥 0,0 썻단 것만 기억난다..
+heappush(q, (0, 0))
+distance = [int(1e9)] * (d+1)
 distance[0] = 0
 
 while q:
-    node, now_dist = q.popleft()
-    for end, dist in graph[node]:
-        cost = dist + distance[node]
-        if cost < distance[end]:
-            distance[end] = cost
-            q.append((end, cost))
+    start, dist = heappop(q)
+    for end, cost in graph[start]:
+        c = dist + cost
+        if c < distance[end]:
+            heappush(q, (end, c))
+            distance[end] = c
+
 print(distance[-1])
