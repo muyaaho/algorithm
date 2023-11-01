6d3c3e7c3ce3b4d76bf8772a8422050704e64ab6
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-01 15:41:01 +0900

    [Gold V] Title: LCS, Time: 548 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,16 @@
+a = input()
+b = input()
+
+width = len(a)+1
+length = len(b)+1
+
+dp = [[0] * length for _ in range(width)]
+
+for i in range(1, width):
+    for j in range(1, length):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(max(dp[-1]))
