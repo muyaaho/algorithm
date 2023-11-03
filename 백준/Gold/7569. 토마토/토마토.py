84847326a9984248dbd65cf59c856d5917181387
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-03 11:46:01 +0900

    [Gold V] Title: 토마토, Time: 3472 ms, Memory: 48460 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,51 @@
+from collections import deque
+import sys
+input =sys.stdin.readline
+
+
+mv = [(1, 0, 0), (0, 0, 1), (0, 1, 0), (-1, 0, 0), (0, 0, -1), (0, -1, 0)]
+
+
+INF = int(1e9)
+n, m, h = map(int, input().split())
+graph = []
+zeroCount = 0
+for i in range(h):
+    arr = []
+    for _ in range(m):
+        line = list(map(int, input().split()))
+        zeroCount += line.count(0)
+        arr.append(line)
+    graph.append(arr)
+
+if zeroCount == 0:
+    print(0)
+    exit(0)
+    
+q = deque()
+
+for k in range(h):
+    for i in range(m):
+        for j in range(n):
+            if graph[k][i][j] == 1:
+                q.append((k,i,j))
+
+while q:
+    k, i, j = q.popleft()
+    for dk, di, dj in mv:
+        nk = dk + k
+        ni = di + i
+        nj = dj + j
+        if 0 <= nk < h and 0 <= ni < m and 0 <= nj <n and graph[nk][ni][nj] == 0:
+            graph[nk][ni][nj] = graph[k][i][j] + 1
+            q.append((nk, ni, nj))
+
+answer = -1
+for box in graph:
+    for line in box:
+        answer = max(answer, max(line))
+        if line.count(0)>0:
+            print(-1)
+            exit(0)
+# print(graph)/
+print(answer-1)
