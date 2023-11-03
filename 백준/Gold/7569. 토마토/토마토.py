0a9d62840eb72703c31d51cfbe64fdfb12db3174
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-03 11:50:23 +0900

    [Gold V] Title: 토마토, Time: 3356 ms, Memory: 48532 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -2,33 +2,30 @@ from collections import deque
 import sys
 input =sys.stdin.readline
 
-
 mv = [(1, 0, 0), (0, 0, 1), (0, 1, 0), (-1, 0, 0), (0, 0, -1), (0, -1, 0)]
-
 
 INF = int(1e9)
 n, m, h = map(int, input().split())
 graph = []
+
 zeroCount = 0
 for i in range(h):
-    arr = []
-    for _ in range(m):
-        line = list(map(int, input().split()))
-        zeroCount += line.count(0)
-        arr.append(line)
+    arr = [list(map(int, input().split())) for _ in range(m)]
     graph.append(arr)
 
-if zeroCount == 0:
-    print(0)
-    exit(0)
-    
 q = deque()
 
 for k in range(h):
     for i in range(m):
         for j in range(n):
+            if graph[k][i][j] == 0:
+                zeroCount += 1
             if graph[k][i][j] == 1:
                 q.append((k,i,j))
+
+if zeroCount == 0:
+    print(0)
+    exit(0)
 
 while q:
     k, i, j = q.popleft()
