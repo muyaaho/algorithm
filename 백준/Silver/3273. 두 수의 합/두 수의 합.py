4309c7cb980241da333a2e6e55d61ec4c4b1596d
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-04 09:55:48 +0900

    [Silver III] Title: 두 수의 합, Time: 76 ms, Memory: 43680 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 x = int(input())
+sarr = set(arr)
 
 arr.sort()
 answer = 0
@@ -11,7 +12,7 @@ answer = 0
 for i in range(n):
     if arr[i] > x//2 or (x%2==0 and arr[i] == x//2):
         break
-    if (x - arr[i] in arr):
+    if (x - arr[i] in sarr):
         answer += 1
 
 print(answer)
