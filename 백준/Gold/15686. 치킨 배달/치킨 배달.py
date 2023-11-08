7c52425370f04f05dade91b3aa71c4e7b53c1df9
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-08 20:29:42 +0900

    [Gold V] Title: 치킨 배달, Time: 584 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,8 +1,12 @@
 from itertools import combinations
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-house, chicken = [], []
+house = []
+chicken =[]
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 1:
@@ -10,14 +14,14 @@ for i in range(n):
         elif arr[i][j] == 2:
             chicken.append((i, j))
 
-choosechi = combinations(chicken, m)
-answer = 1e9
-for x in choosechi:
-    city_chi = 0
+ans = []
+for coordinates in combinations(chicken, m):
+    # 모든 집마다 치킨 m개씩 돌면서 치킨 최소인 치킨 거리 구하고 이걸 도시의 치킨거리에 더하기
+    dist_chi = 0
     for hx, hy in house:
-        min_chi = 999999
-        for axis in x:
-            min_chi = min(min_chi, abs(hx-axis[0])+abs(hy-axis[1]))
-        city_chi += min_chi
-    answer = min(answer, city_chi)
-print(answer)
+        min_chi = 9999
+        for cx, cy in coordinates:
+            min_chi = min(min_chi, abs(hx-cx) + abs(hy-cy))
+        dist_chi += min_chi
+    ans.append(dist_chi)
+print(min(ans))
