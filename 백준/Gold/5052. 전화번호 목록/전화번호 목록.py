67adeb544ce480a6cf215b728858fc4d26928047
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-09 11:36:28 +0900

    [Gold IV] Title: 전화번호 목록, Time: 156 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -3,10 +3,12 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     n = int(input())
-    nums = sorted(list(input().rstrip() for _ in range(n)))
+    arr = sorted([input().rstrip() for _ in range(n)])
+    
     isNo = False
     for i in range(n-1):
-        if nums[i+1].find(nums[i]) == 0:
+        # print(arr[i], arr[i+1], arr[i+1].find(arr[i]))
+        if arr[i+1].find(arr[i]) == 0:
             isNo = True
             break
     print('NO' if isNo else 'YES')
