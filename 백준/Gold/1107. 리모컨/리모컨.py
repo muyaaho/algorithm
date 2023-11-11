a4ea5df86ab107e26de7d9fde0b49d6317b7a42e
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-11 11:10:00 +0900

    [Gold V] Title: 리모컨, Time: 312 ms, Memory: 110616 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,15 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 m = int(input())
 broken = set(input().split()) if m else set()
-answer = abs(100-n)
+ans = abs(100-n)
 
-# nums가 지금 나온 숫자(누른 리모컨 번호)
 for nums in range(1_000_001):
     for num in str(nums):
         if num in broken:
             break
     else:
-        # 버튼 누른 횟수 + 차이점(+, - 이동)
-        answer = min(answer, len(str(nums)) + abs(nums-n))
+        ans = min(ans, len(str(nums)) + abs(n - nums))
 
-print(answer)
+print(ans)
