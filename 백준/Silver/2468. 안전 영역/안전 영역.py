dc60b7f742cb6822c3ca16ee220a6c9f94713be9
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-13 10:53:08 +0900

    [Silver I] Title: 안전 영역, Time: 760 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,3 +1,4 @@
+
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -5,20 +6,18 @@ input = sys.stdin.readline
 n = int(input())
 arr = []
 max_level = -1
-# min_level = 1e9
 for _ in range(n):
     line = list(map(int, input().split()))
     max_level = max(max_level, max(line))
-    # min_level = min(min_level, min(line))
     arr.append(line)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+
 def bfs(level, x, y):
     q = deque()
     q.append((x, y))
     visited[x][y] = True
-    isNext = False
     while q:
         x, y = q.popleft()
         for i in range(4):
@@ -27,11 +26,9 @@ def bfs(level, x, y):
             
             if 0 <= nx < n and 0 <= ny < n:
                 if not visited[nx][ny] and arr[nx][ny] > level:
-                    isNext = True
                     visited[nx][ny] = True
                     q.append((nx, ny))
 
-    return isNext
 
 answer = 0
 
