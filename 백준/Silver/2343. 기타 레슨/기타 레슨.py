254cdead5ae0e42f86ba5d77c8651eb7da7d1e5b
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-14 09:33:18 +0900

    [Silver I] Title: 기타 레슨, Time: 444 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -5,20 +5,20 @@ start = max(arr)
 end = sum(arr)
 
 while start <= end:
-    mid = (start+end)//2
-    
+    mid = (start + end)//2
     cnt, s = 0, 0
+    
     for x in arr:
-        if x + s > mid:
+        if s+x > mid:
             cnt += 1
             s = 0
         s += x
     if s:
         cnt += 1
     
-    if cnt <= m:
-        end = mid-1
+    if cnt > m:
+        # 자른 것 보다 갯수가 많이 나오면 너무 작게 잘랐다는 말임. (분이 작다) 그래서 start
+        start = mid+1
     else:
-        start = mid + 1
-        
+        end = mid-1
 print(start)
