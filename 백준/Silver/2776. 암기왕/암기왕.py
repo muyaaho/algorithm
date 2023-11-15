eb827544f14fe0a614d391052f24266fe046f6cf
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-15 10:14:17 +0900

    [Silver IV] Title: 암기왕, Time: 1524 ms, Memory: 222500 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    n = int(input())
+    arr1 = set(map(int, input().split()))
+    m = int(input())
+    arr2 = list(map(int, input().split()))  
+    
+    for x in arr2:
+        print(1 if x in arr1 else 0)
