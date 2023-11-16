688e3a3ef13c46e5ddcc6be474b76996e9b2d1e5
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-16 16:33:09 +0900

    [Silver II] Title: 친구, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -1,17 +1,22 @@
 n = int(input())
 arr = [list(input()) for _ in range(n)]
 
-connected = [[0] * n for _ in range(n)]
+INF = int(1e9)
+graph = [[INF] * n for _ in range(n)]
+
 for k in range(n):
     for a in range(n):
         for b in range(n):
-            if a == b:
+            if a==b:
                 continue
             if arr[a][b] == 'Y' or (arr[a][k] == 'Y' and arr[k][b] == 'Y'):
-                connected[a][b] = 1
+                graph[a][b] = 1
 
-# print(connected)
 answer = 0
-for line in connected:
-    answer = max(answer, sum(line))
+for i in range(n):
+    count = 0
+    for j in range(n):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    answer = max(answer, count)
 print(answer)
