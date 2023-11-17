23523689220caf550a320e7e40a14ce31c2d4523
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-17 17:20:45 +0900

    [Silver II] Title: 친구, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -1,22 +1,20 @@
 n = int(input())
 arr = [list(input()) for _ in range(n)]
-
 INF = int(1e9)
+
 graph = [[INF] * n for _ in range(n)]
 
 for k in range(n):
     for a in range(n):
         for b in range(n):
-            if a==b:
+            if a == b:
                 continue
             if arr[a][b] == 'Y' or (arr[a][k] == 'Y' and arr[k][b] == 'Y'):
                 graph[a][b] = 1
 
 answer = 0
-for i in range(n):
-    count = 0
-    for j in range(n):
-        if graph[i][j] != INF or graph[j][i] != INF:
-            count += 1
+# print(graph)
+for line in graph:
+    count = line.count(1)
     answer = max(answer, count)
 print(answer)
