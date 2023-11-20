4ceb010526a92aad0b8b9fb84d172f84ee88a356
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-20 17:55:52 +0900

    [Gold IV] Title: 키 순서, Time: 2368 ms, Memory: 113284 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.py
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -1,11 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+INF = int(1e9)
 n, m = map(int, input().split())
 
-INF = int(1e9)
 graph = [[INF] * (n+1) for _ in range(n+1)]
-
 for a in range(n+1):
     graph[a][a] = 0
 
@@ -19,9 +18,9 @@ for k in range(n+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
 answer = 0
-for i in range(1, n+1):
+for i in range(n+1):
     count = 0
-    for j in range(1,n+1):
+    for j in range(n+1):
         if graph[i][j] != INF or graph[j][i] != INF:
             count += 1
     if count == n:
