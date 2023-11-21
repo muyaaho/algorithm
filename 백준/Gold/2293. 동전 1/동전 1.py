27ad682dbef62aa3b9f2fb2436c4873eae6a7006
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-21 10:24:15 +0900

    [Gold V] Title: 동전 1, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 arr.sort()
@@ -9,6 +6,6 @@ dp = [0] * (k+1)
 dp[0] = 1
 for money in arr:
     for i in range(money, k+1):
-        if i-money >= 0:
-            dp[i] += dp[i-money]
+        dp[i] += dp[i-money]
+
 print(dp[k])
