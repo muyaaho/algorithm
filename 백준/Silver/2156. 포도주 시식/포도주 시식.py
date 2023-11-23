5c983ec5356ff616d14ef72221779b1e4639b8e1
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-23 10:07:45 +0900

    [Silver I] Title: 포도주 시식, Time: 408 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,13 +1,15 @@
 n = int(input())
-arr = [0] * 10001
+arr = [0] * 10000
+
 for i in range(n):
     arr[i] = int(input())
 
-dp = [0] * 10_001
+dp =[0] * 10000
 dp[0] = arr[0]
 dp[1] = arr[0] + arr[1]
-dp[2] = max(arr[1] + arr[2], arr[0] + arr[2], dp[1])
+dp[2] = max(arr[0] + arr[2], arr[1] + arr[2], dp[1])
 
-for i in range(3, n+1):
-    dp[i] = max(dp[i-2] + arr[i], dp[i-1], arr[i] + arr[i-1] + dp[i-3])
-print(max(dp))
+for i in range(3, n):
+    dp[i] = max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i], dp[i-1])
+
+print(dp[n-1])
