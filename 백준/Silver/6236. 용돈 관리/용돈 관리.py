a20118e2f4bb7cb49436b4704051d4e52208abbe
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-29 10:31:40 +0900

    [Silver II] Title: 용돈 관리, Time: 492 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -1,24 +1,28 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-daily = list(int(input()) for _ in range(n))
-lt = min(daily)
-rt = sum(daily)
+arr = [int(input()) for _ in range(n)]
 
-# 이분탐색 시작
-while lt <= rt:
-    mid = (lt + rt) // 2		# 중간값. 인출할 금액
-    charge = mid			# 현재 가진 돈. 처음 인출.
-    num = 1				# 인출 횟수
-    for i in range(n):			# n일 살기 시작
-        if charge < daily[i]:		# 가진 돈이 부족하면 돈 인출
-            charge = mid
-            num += 1
-        charge -= daily[i]		# 그날 다 살음
+start = 0
+end = sum(arr)
 
-# m번보다 더 많이 인출하거나 인출한 금액이 하루를 다 살기에 적은 경우 (인출 금액이 적음)
-    if num > m or mid < max(daily):	
-        lt = mid + 1
-    else:				# 인출 횟수가 m번보다 적거나 같음 (인출 금액이 많음)
-        rt = mid - 1
-        k = mid				# k값 저장
-
-print(k)
+answer =0
+while start <= end:
+    mid = (start + end)//2
+    s = mid
+    cnt = 1
+    
+    for money in arr:
+        if s < money:
+            cnt += 1
+            s = mid
+        s -= money
+        # print(s)
+    
+    if cnt > m or mid < max(arr):
+        start = mid+1
+    else:
+        end = mid-1
+        answer = mid
+print(answer)
