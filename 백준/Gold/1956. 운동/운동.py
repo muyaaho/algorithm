3f530c5328784e2a2aac6370657f123c4a3941e1
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-29 10:56:37 +0900

    [Gold IV] Title: 운동, Time: 980 ms, Memory: 112732 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -2,7 +2,6 @@ v, e = map(int, input().split())
 INF = int(1e9)
 graph = [[INF] * (v+1) for _ in range(v+1)]
 
-    
 for _ in range(e):
     a, b, c = map(int, input().split())
     graph[a][b] = c
@@ -13,6 +12,6 @@ for k in range(1, v+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
 answer = INF
-for a in range(1, v+1):
-    answer = min(graph[a][a], answer)
-print(-1 if answer >= INF else answer)
+for i in range(1, v+1):
+    answer = min(answer, graph[i][i])
+print(answer if answer < INF else -1)
