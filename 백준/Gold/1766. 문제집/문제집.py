feb2a065492cd9a3815b3359eb0c7eca5a193390
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-11-30 22:27:41 +0900

    [Gold II] Title: 문제집, Time: 212 ms, Memory: 39584 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,25 @@
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+indegree = [0] * (n+1)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+q = []
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        heappush(q, i)
+        
+while q:
+    now = heappop(q)
+    print(now, end=' ')
+    for next in graph[now]:
+        indegree[next] -= 1
+        if indegree[next] == 0:
+            heappush(q, next)
