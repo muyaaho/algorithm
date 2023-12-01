53700e115ae9c60d94c1648d2999c354d20cfb9b
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-12-01 11:27:39 +0900

    [Silver I] Title: 행렬, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+A = [list(map(int, input())) for _ in range(n)]
+B = [list(map(int, input())) for _ in range(n)]
+
+def check():
+    for i in range(n):
+        for j in range(m):
+            if A[i][j] != B[i][j]:
+                return False
+    return True
+
+def reverse(x, y):
+    for i in range(x, x+3):
+        for j in range(y, y+3):
+            A[i][j] = 1-A[i][j]
+
+answer = 0
+for i in range(n-2):
+    for j in range(m-2):
+        if A[i][j] != B[i][j]:
+            reverse(i, j)
+            answer += 1
+
+print(answer if check() else -1)
