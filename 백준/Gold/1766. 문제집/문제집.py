260eefe28df5918d98089a9cadae788a827d6726
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-12-01 11:22:40 +0900

    [Gold II] Title: 문제집, Time: 204 ms, Memory: 39584 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -6,6 +6,7 @@ n, m = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
 indegree = [0] * (n+1)
+
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
@@ -15,7 +16,7 @@ q = []
 for i in range(1, n+1):
     if indegree[i] == 0:
         heappush(q, i)
-        
+
 while q:
     now = heappop(q)
     print(now, end=' ')
