2e1ebcc4bd364df929a147f333ef42da4baa8765
서지희 <76798969+muyaaho@users.noreply.github.com>
2023-12-05 11:18:27 +0900

    [Gold III] Title: 내리막 길, Time: 136 ms, Memory: 42780 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,16 +1,20 @@
+import sys
+
+input = sys.stdin.readline
 m, n = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(m)]
 
 dp = [[-1] * n for _ in range(m)]
 dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
+dy = [-1, 1, 0, 0]
+
 
 def dfs(x, y):
-    if x == m-1 and y == n-1:
+    if x == m - 1 and y == n - 1:
         return 1
     if dp[x][y] != -1:
         return dp[x][y]
-    
+
     ways = 0
     for i in range(4):
         nx = x + dx[i]
@@ -19,5 +23,6 @@ def dfs(x, y):
             ways += dfs(nx, ny)
     dp[x][y] = ways
     return dp[x][y]
+
 
 print(dfs(0, 0))
