c2e10b0382606dce29b360e46c3285843736fa8e
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-01-05 14:40:33 +0900

    [Silver II] Title: 사탕 게임, Time: 3180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,35 +1,36 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-arr = [list(input().rstrip()) for _ in range(n)]
-answer = 0
+arr = [list(input()) for _ in range(n)]
 
+answer = 0
 def check():
     global answer
+    
     for i in range(n):
         cnt = 1
         for j in range(1, n):
             if arr[i][j] == arr[i][j-1]:
                 cnt += 1
-                answer = max(cnt, answer)
+                answer = max(answer, cnt)
             else:
                 cnt = 1
         cnt = 1
         for j in range(1, n):
             if arr[j][i] == arr[j-1][i]:
                 cnt += 1
-                answer = max(cnt, answer)
+                answer = max(answer, cnt)
             else:
                 cnt = 1
+        
+        
+
 
 for i in range(n):
     for j in range(n):
-        if j + 1 < n:
+        if j+1 < n:
             arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
             check()
             arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-        if i + 1  < n:
+        if i+1 < n:
             arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
             check()
             arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
