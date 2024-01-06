80f1d41cd8a2482fb80a9b0e5ca3d877c0205f99
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-01-06 22:14:04 +0900

    [Silver IV] Title: 상근이의 여행, Time: 152 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    edges = []
+    
+    for _ in range(m):
+        a, b = map(int, input().split())
+    
+    print(n-1)
