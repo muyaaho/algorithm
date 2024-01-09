b82b3e6c9528dcf4716f98433ed1d0aeaf5c8c44
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-01-09 17:57:42 +0900

    [Silver II] Title: 사탕 게임, Time: 3180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,36 +1,32 @@
 n = int(input())
 arr = [list(input()) for _ in range(n)]
 
-answer = 0
 def check():
     global answer
-    
     for i in range(n):
         cnt = 1
         for j in range(1, n):
             if arr[i][j] == arr[i][j-1]:
                 cnt += 1
-                answer = max(answer, cnt)
+                answer = max(cnt, answer)
             else:
                 cnt = 1
         cnt = 1
         for j in range(1, n):
             if arr[j][i] == arr[j-1][i]:
                 cnt += 1
-                answer = max(answer, cnt)
+                answer = max(cnt, answer)
             else:
                 cnt = 1
-        
-        
 
-
+answer = 0
 for i in range(n):
     for j in range(n):
-        if j+1 < n:
+        if j + 1 < n:
             arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
             check()
             arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-        if i+1 < n:
+        if i + 1 < n:
             arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
             check()
             arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
