9a1e8796673ced27122922edc6f8f6a4188f9416
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-01-09 19:32:26 +0900

    [Gold II] Title: 전기가 부족해, Time: 276 ms, Memory: 46628 KB -BaekjoonHub

--- 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
@@ -1,10 +1,13 @@
 import sys
+
 input = sys.stdin.readline
+
 
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
+
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -14,9 +17,10 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
+
 n, m, k = map(int, input().split())
 arr = list(map(int, input().split()))
-parents = [i for i in range(n+1)]
+parents = [i for i in range(n + 1)]
 edges = []
 for _ in range(m):
     u, v, w = map(int, input().split())
