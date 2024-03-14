3a18efb4d74e20f3936fb310ec5b4e1e560a8fd6
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-14 09:31:07 +0900

    [Gold IV] Title: 뱀, Time: 64 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,57 +1,61 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 k = int(input())
 
 graph = [[0] * n for _ in range(n)]
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
 
-for i in range(k):
-    a, b= map(int, input().split())
-    graph[a-1][b-1] = 2
+for _ in range(k):
+    x, y = map(int, input().split())
+    graph[x-1][y-1] = 2
 
 l = int(input())
-d = dict()
-q = deque()
-q.append((0, 0))
 
-for i in range(l):
-    x, c = input().split()
-    d[int(x)] = c
+direct_info = dict()
+coords = deque()
+coords.append((0, 0))
 
+for _ in range(l):
+    x, c = input().rstrip().split()
+    direct_info[int(x)] = c
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 x, y = 0, 0
 graph[x][y] = 1
-cnt = 0
+time = 0
 direction = 0
 
-def turn(alpha):
+def turn(direct):
     global direction
-    if alpha == 'L':
-        direction = (direction - 1) % 4
+    if direct == 'L':
+        direction = (direction - 1)%4
     else:
-        direction = (direction + 1) % 4
+        direction = (direction + 1)%4
 
 while True:
-    cnt += 1
+    time += 1
     x += dx[direction]
     y += dy[direction]
-    
     if not (0 <= x < n and 0 <= y < n):
         break
     
     if graph[x][y] == 2:
+        coords.append((x, y))
         graph[x][y] = 1
-        q.append((x, y))
-        if cnt in d:
-            turn(d[cnt])
     elif graph[x][y] == 0:
+        coords.append((x, y))
         graph[x][y] = 1
-        q.append((x, y))
-        tx, ty = q.popleft()
+        tx, ty = coords.popleft()
         graph[tx][ty] = 0
-        if cnt in d:
-            turn(d[cnt])
     else:
         break
+    
+    
+    
+    if time in direct_info:
+        turn(direct_info[time])
 
-print(cnt)
+print(time)
