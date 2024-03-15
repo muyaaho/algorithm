24bac22f1ce86d1611b18dfd78c91aa3cce483b4
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-15 09:48:01 +0900

    [Silver II] Title: 사탕 게임, Time: 3152 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,34 +1,46 @@
+'''
+색이 다른 인접한 두 칸을 고르는 모든 경우의 수를 구함
+한 번만 색상 변경
+그리고 그 중에서 가장 긴 연속 부분인 행 or 열의 사탕 개수
+'''
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = [list(input()) for _ in range(n)]
+board = [list(input().rstrip()) for _ in range(n)]
+
+answer = 0
 
 def check():
     global answer
     for i in range(n):
         cnt = 1
         for j in range(1, n):
-            if arr[i][j] == arr[i][j-1]:
+            if board[i][j-1] == board[i][j]:
                 cnt += 1
-                answer = max(cnt, answer)
+                answer = max(answer, cnt)
             else:
                 cnt = 1
         cnt = 1
         for j in range(1, n):
-            if arr[j][i] == arr[j-1][i]:
+            if board[j][i] == board[j-1][i]:
                 cnt += 1
-                answer = max(cnt, answer)
+                answer = max(answer, cnt)
             else:
                 cnt = 1
+                
 
-answer = 0
+
 for i in range(n):
     for j in range(n):
-        if j + 1 < n:
-            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
-            check()
-            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
         if i + 1 < n:
-            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
             check()
-            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+        if j + 1 < n:
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+            check()
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
 
 print(answer)
