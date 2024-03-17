df3cc3db4691c920151d2fdce77e2af8907fb3de
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-18 08:59:18 +0900

    [Gold V] Title: 동전 2, Time: 476 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,19 +1,12 @@
-import sys
-
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
+
 arr = [int(input()) for _ in range(n)]
 
-d = [10001] * (k+1)
-
-d[0] = 0
+dp = [10001] * (k+1)
+dp[0] = 0
 for i in range(n):
-    for j in range(arr[i], k+1):
-        if d[j-arr[i]] != 10001:
-            d[j] = min(d[j], d[j - arr[i]] + 1)
+    for j in range(arr[i], k + 1):
+        if dp[j - arr[i]] != 10001:
+            dp[j] = min(dp[j], dp[j - arr[i]] + 1)
 
-if d[k] == 10001:
-    print(-1)
-else:
-    print(d[k])
+print(-1 if dp[k] > 10000 else dp[k])
