4185d8dd0ea1d94494ed23da98cd7d08fbe1ddc7
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-17 21:04:00 +0900

    [Gold III] Title: 아기 상어, Time: 212 ms, Memory: 34704 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -6,77 +6,66 @@ input = sys.stdin.readline
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-# baby shark
-bx, by = 0, 0
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 9:
             bx, by = i, j
             break
 
+dx = [-1, 0,1 , 0]
+dy = [0, -1, 0, 1]
+
 target = 2
 eat_count = 0
 answer = 0
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+def print_map():
+    for line in arr:
+        print(line)
+    print()
 
-def bfs(start):
+def bfs(sx, sy):
     q = deque()
-    q.append(start)
+    q.append((sx, sy))
     visited = [[-1] * n for _ in range(n)]
-    visited[start[0]][start[1]] = 0
+    visited[sx][sy] = 0
     
     while q:
-        
         x, y = q.popleft()
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if (0 <= nx < n and 0 <= ny < n) and visited[nx][ny] < 0 and arr[nx][ny] <= target:
-                visited[nx][ny] = visited[x][y] + 1
+            
+            if (0 <= nx < n and 0 <= ny < n) and arr[nx][ny] <= target and visited[nx][ny] < 0:
                 q.append((nx, ny))
-    # 모든 좌표의 최소 거리 
+                visited[nx][ny] = visited[x][y] + 1
+    
     return visited
-                
-while True:
-    info = dict()
-    hasFish = False
-    dist_map = bfs((bx, by))
 
+while True:
+    
+    dist_map = bfs(bx, by)
+    dist = []
+    can_eat = 0
+    
     for i in range(n):
         for j in range(n):
-            if eat_count < target and arr[i][j] < target and arr[i][j] > 0:
-                hasFish = True
-                cnt = dist_map[i][j]
-                if cnt == -1:
-                    continue
-
-                if not cnt in info:
-                    info[cnt] = [(i, j)]
-                else:
-                    heappush(info[cnt], (i, j))
+            if arr[i][j] < target and arr[i][j] > 0 and dist_map[i][j] > 0:
+                heappush(dist, (dist_map[i][j], i, j))
     
-    if not hasFish or not info:
+    if not dist:
         print(answer)
         break
     
-    d1 = sorted(info.items())
-    go_cnt, axis = d1[0]
-    
-    if len(axis) == 1:
-        fx, fy = axis[0][0], axis[0][1]
-    else:
-        fx, fy = heappop(info[go_cnt])
-    answer += go_cnt
-    
-    arr[fx][fy] = 9
-    arr[bx][by] = 0
-    bx, by = fx, fy
-    
+    _, ex, ey = heappop(dist)
+    answer += dist_map[ex][ey]
     eat_count += 1
+    
     if eat_count == target:
         target += 1
         eat_count = 0
     
+    arr[ex][ey] = 9
+    arr[bx][by] = 0
+    bx, by = ex, ey
