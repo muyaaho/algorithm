7a92898e1eaf55db0c5893a2ecbf26e27b3ae4b8
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-17 21:23:34 +0900

    [Gold III] Title: 세 용액, Time: 4532 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,5 +1,4 @@
 import sys
-
 input = sys.stdin.readline
 
 n = int(input())
@@ -8,21 +7,22 @@ arr = list(map(int, input().split()))
 arr.sort()
 mintake = sys.maxsize
 
-for i in range(n - 2):
+for i in range(n-1):
     start = i + 1
-    end = n - 1
-
+    end = n-1
+    
     while start < end:
         take = arr[i] + arr[start] + arr[end]
-
+        
         if abs(take) < mintake:
             mintake = abs(take)
-            result = [arr[i], arr[start], arr[end]]
-
+            ans = [arr[i], arr[start] , arr[end]]
+        
         if take < 0:
             start += 1
         elif take > 0:
             end -= 1
         else:
             break
-print(*result)
+
+print(*ans)
