b23cacd99276d54a5c063c1c184a8344cc33a277
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-24 12:00:14 +0900

    [Gold IV] Title: 인구 이동, Time: 5276 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -4,57 +4,46 @@ input = sys.stdin.readline
 
 n, l, r = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-count = 0
+
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-# 좌표를 구함
 def bfs(startx, starty):
     s = set()
     q = deque([(startx, starty)])
     
+    visited[startx][starty] = True
     
     while q:
         x, y = q.popleft()
-        
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            
-            if 0 <= nx < n and 0 <= ny < n:
-                if l <= abs(arr[x][y] - arr[nx][ny]) <= r and not visited[nx][ny]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if l <= abs(arr[x][y] - arr[nx][ny]) <= r:
                     q.append((nx, ny))
-                    visited[nx][ny] = True
-                    visited[x][y] = True
                     s.add((nx, ny))
                     s.add((x, y))
-    
+                    visited[nx][ny] = True
     return s
 
-is_end = True
+answer = 0
 while True:
-    visited = [[False] * n for _ in range(n)]
     set_list = []
-    # print(count)
+    visited = [[False] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                s = bfs(i, j)
-                if s:
+                if s:=bfs(i, j):
                     set_list.append(s)
-
     
     if set_list:
+        answer += 1
         for s in set_list:
-            avg_sum = sum([arr[x][y] for x, y in s])
-            # print(avg_sum)
-            avg = avg_sum // len(s)
-            # print(len(s))
-            for i, j in s:
-                arr[i][j] = avg
+            avg = sum([arr[x][y] for x, y in s]) // len(s)
+            for x, y in s:
+                arr[x][y] = avg
     else:
         break
 
-    count += 1
-
-print(count)
+print(answer)
