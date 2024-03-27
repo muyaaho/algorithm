3c3194f6b73237f5e22b9f37ec2e906ba9b38d29
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-27 16:55:04 +0900

    [Gold V] Title: 센서, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -5,13 +5,9 @@ n = int(input())
 k = int(input())
 arr = sorted(map(int, input().split()))
 
-if n <= k:
-    print(0)
-else:
-    dist = []
-    for i in range(n-1):
-        dist.append(abs(arr[i] - arr[i+1]))
+dist = []
+for i in range(n-1):
+    dist.append(arr[i+1] - arr[i])
 
-    dist.sort(reverse=True)
-    # print(dist[(k-1):])
-    print(sum(dist[(k-1):]))
+dist.sort(reverse = True)
+print(sum(dist[k-1:]))
