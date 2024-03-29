df1ae440456bb5c098d4114af2e9a493d6ade009
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-29 10:08:30 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 312 ms, Memory: 57372 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,24 +1,21 @@
 from heapq import heappush, heappop
 import sys
-
 input = sys.stdin.readline
-INF = int(1e9)
 
 n = int(input())
-m = int(input())
+graph = [[] for _ in range(n+1)]
 
-graph = [[] for _ in range(n + 1)]
-for _ in range(m):
-    a, b, cost = map(int, input().split())
-    graph[a].append((cost, b))
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
 start, end = map(int, input().split())
 
 q = []
+parent = [0] * (n+1)
 heappush(q, (0, start))
-distance = [INF] * (n+1)
+distance = [int(1e9)] * (n+1)
 distance[start] = 0
-parents = [0] * (n+1)
 
 while q:
     dist, now = heappop(q)
@@ -26,20 +23,21 @@ while q:
     if dist > distance[now]:
         continue
     
-    for cost, next in graph[now]:
+    for next, cost in graph[now]:
         c = cost + dist
         if c < distance[next]:
             distance[next] = c
-            parents[next] = now
+            parent[next] = now
             heappush(q, (c, next))
 
 print(distance[end])
 
-path = []  # 경로
+path = []
 curr = end
 while curr:
     path.append(curr)
-    curr = parents[curr]
+    curr = parent[curr]
+
 print(len(path))
-for i in path[::-1]:  # 경로 출력
-    print(i, end=" ")
+print(*reversed(path), end=' ')
+    
