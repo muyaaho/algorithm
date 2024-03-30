2ee295728a64dcfd4de5ab69ac9c14bb8738f1bb
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-03-30 15:10:56 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 308 ms, Memory: 57396 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -3,23 +3,23 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+m = int(input())
 graph = [[] for _ in range(n+1)]
 
-for _ in range(int(input())):
+for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
 
 start, end = map(int, input().split())
 
 q = []
-parent = [0] * (n+1)
 heappush(q, (0, start))
+parent = [0] * (n+1)
 distance = [int(1e9)] * (n+1)
 distance[start] = 0
 
 while q:
     dist, now = heappop(q)
-    
     if dist > distance[now]:
         continue
     
@@ -27,17 +27,15 @@ while q:
         c = cost + dist
         if c < distance[next]:
             distance[next] = c
-            parent[next] = now
             heappush(q, (c, next))
-
-print(distance[end])
+            parent[next] = now
 
 path = []
 curr = end
 while curr:
     path.append(curr)
     curr = parent[curr]
-
+    
+print(distance[end])
 print(len(path))
 print(*reversed(path), end=' ')
-    
