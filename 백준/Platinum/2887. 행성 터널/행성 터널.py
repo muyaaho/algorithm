c63663a680928fb65bba7b49d74b931cfde48113
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-01 10:53:28 +0900

    [Platinum V] Title: 행성 터널, Time: 1188 ms, Memory: 100536 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -16,7 +16,8 @@ def union_parent(parent, a, b):
 
 n = int(input())
 x, y, z = [], [], []
-for i in range(1, n+1):
+
+for i in range(1, n + 1):
     a, b, c = map(int, input().split())
     x.append((a, i))
     y.append((b, i))
@@ -28,20 +29,17 @@ z.sort()
 
 edges = []
 for i in range(n-1):
-    edges.append(((x[i+1][0] - x[i][0]), x[i][1], x[i+1][1]))
-    edges.append(((y[i+1][0] - y[i][0]), y[i][1], y[i+1][1]))
-    edges.append(((z[i+1][0] - z[i][0]), z[i][1], z[i+1][1]))
+    edges.append((x[i+1][0] - x[i][0], x[i+1][1], x[i][1]))
+    edges.append((y[i+1][0] - y[i][0], y[i+1][1], y[i][1]))
+    edges.append((z[i+1][0] - z[i][0], z[i+1][1], z[i][1]))
 
 edges.sort()
-answer = 0
+
 parent = [i for i in range(n+1)]
-cnt = 0
+answer = 0
 for cost, a, b in edges:
     if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
         answer += cost
-        cnt += 1
-    if cnt == n:
-        break
 
 print(answer)
