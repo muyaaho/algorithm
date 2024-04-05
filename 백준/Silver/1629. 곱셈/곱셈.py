f472010ca6855320b4c66090aa72113abc316afa
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-05 15:45:12 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,15 +1,12 @@
-import sys
-input = sys.stdin.readline
-
 a, b, c = map(int, input().split())
 
-def dc(b):
+def bf(b):
     if b == 1:
-        return a % c
-
+        return a%c
+    
     if b % 2 == 0:
-        return ((dc(b//2) % c)**2)%c
-    else:
-        return ((dc(b//2) % c) ** 2 * a)%c
+        return ((bf(b//2) % c) ** 2) % c
+    
+    return ((bf(b//2) % c) ** 2 * a) % c
 
-print(dc(b))
+print(bf(b))
