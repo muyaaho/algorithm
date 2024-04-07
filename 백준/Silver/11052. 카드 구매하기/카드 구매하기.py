a1daa5a3a04675dd1bce17d4125c565a6f9d3797
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-07 19:04:54 +0900

    [Silver I] Title: 카드 구매하기, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] + list(map(int, input().split()))
+
+for i in range(1, n+1):
+    for j in range(1, i):
+        if dp[i] < dp[j] + dp[i-j]:
+            dp[i] = dp[j] + dp[i-j]
+
+print(dp[n])
