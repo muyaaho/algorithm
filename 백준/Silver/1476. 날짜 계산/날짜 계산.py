c0048a3584636b70490b11c795e7b346e60da9ce
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-17 22:41:48 +0900

    [Silver V] Title: 날짜 계산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,12 @@
+e, s, m = map(int, input().split())
+i = 1
+
+while True:
+    ne = 15 if i % 15 == 0 else i % 15
+    ns = 28 if i % 28 == 0 else i % 28
+    nm = 19 if i % 19 == 0 else i % 19
+
+    if ne == e and ns == s and nm == m:
+        print(i)
+        break
+    i += 1
