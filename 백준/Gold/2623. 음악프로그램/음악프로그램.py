2b5fd2097dcc7ee05198598f15aa9a81d68f49d9
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-21 22:31:45 +0900

    [Gold III] Title: 음악프로그램, Time: 64 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.py
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.py
@@ -5,29 +5,30 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 indegree = [0] * (n+1)
 graph = [[] for _ in range(n+1)]
-
 for _ in range(m):
     arr = list(map(int, input().split()))
-    for i in range(1, len(arr) - 1):
-        indegree[arr[i+1]] += 1
-        graph[arr[i]].append(arr[i+1])
+    # print(arr)
+    for i in range(1, arr[0]):
+        a, b = arr[i], arr[i+1]
+        # print(a, b)
+        graph[a].append(b)
+        indegree[b] += 1
 
 q = deque()
 for i in range(1, n+1):
     if indegree[i] == 0:
         q.append(i)
 
-ans = []
+answer = []
 while q:
     now = q.popleft()
-    ans.append(now)
-    
+    answer.append(now)
     for next in graph[now]:
         indegree[next] -= 1
         if indegree[next] == 0:
             q.append(next)
 
-if len(ans) == n:
-    print(*ans, sep='\n')
+if len(answer) == n:
+    print(*answer, sep='\n')
 else:
     print(0)
