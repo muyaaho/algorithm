21e1e2f432338b021bf34496f492089df2476d1c
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-22 10:47:58 +0900

    [Gold II] Title: 친구 네트워크, Time: 280 ms, Memory: 61160 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -2,7 +2,6 @@ import sys
 input = sys.stdin.readline
 
 def find_parent(x):
-    # print(parent[x])
     if x != parent[x]:
         parent[x] = find_parent(parent[x])
     return parent[x]
@@ -13,13 +12,12 @@ def union_parent(a, b):
     if a != b:
         parent[b] = a
         number[a] += number[b]
-    print(number[a]) 
-    
+    print(number[a])
+
 for _ in range(int(input())):
-    n = int(input())
+    f = int(input())
     parent, number = {}, {}
-    
-    for _ in range(n):
+    for _ in range(f):
         a, b = input().rstrip().split()
         if a not in parent:
             parent[a] = a
@@ -27,5 +25,4 @@ for _ in range(int(input())):
         if b not in parent:
             parent[b] = b
             number[b] = 1
-        
         union_parent(a, b)
