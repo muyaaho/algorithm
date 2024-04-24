1e7e30fbc9c22991f02536eea73d3608a6a29508
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-24 11:46:17 +0900

    [Gold V] Title: A와 B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,15 +1,16 @@
 import sys
 input = sys.stdin.readline
-s = list(input().rstrip())
-t = list(input().rstrip())
 
-while len(s) != len(t):
-    if t[-1] == 'A':
-        t.pop()
-    elif t[-1] == 'B':
-        t.pop()
-        t.reverse()
+a = list(input().rstrip())
+b = list(input().rstrip())
+
+while len(a) < len(b):
+    if b[-1] == 'A':
+        b.pop()
+    elif b[-1] == 'B':
+        b.pop()
+        b.reverse()
     else:
         break
 
-print(1 if s == t else 0)
+print(1 if a == b else 0)
