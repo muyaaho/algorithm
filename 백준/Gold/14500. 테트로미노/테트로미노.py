29a7ec039bd1e51185f6122b48c88d9115dba578
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-04-24 10:52:15 +0900

    [Gold IV] Title: 테트로미노, Time: 180 ms, Memory: 38000 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -3,6 +3,9 @@ input = sys.stdin.readline
 
 def dfs(cnt, s, tmp):
     global answer
+    if s + (4-cnt) * mx <= answer:
+        return
+    
     if cnt == 4:
         answer = max(answer, s)
         return
@@ -14,14 +17,13 @@ def dfs(cnt, s, tmp):
                 visited[nx][ny] = True
                 dfs(cnt + 1, s + arr[nx][ny], tmp + [(nx, ny)])
                 visited[nx][ny] = False
-                
-            
 
 n, m = map(int, input().split())    
 arr = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 
 answer = 0
+mx = max(map(max, arr))
 
 for i in range(n):
     for j in range(m):
