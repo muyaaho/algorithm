85b3c6fca1e0abdb9c2cd9f16add18da1fc55760
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-06-12 10:00:16 +0900

    [Silver I] Title: 카잉 달력, Time: 2020 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,14 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-def cal(arr):
+
+def f(arr):
     m, n, x, y = arr
     k = x
+    
     while k <= m * n:
         if (k-x) % m == 0 and (k-y) % n == 0:
             return k
         k += m
+    
     return -1
 
 for _ in range(int(input())):
-    print(cal(map(int, input().split())))
+    print(f(map(int, input().split())))
