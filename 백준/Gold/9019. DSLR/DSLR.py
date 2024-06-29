155b373a5733636a6df2110a960ab4fdbca7723d
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-06-29 09:52:36 +0900

    [Gold IV] Title: DSLR, Time: 7640 ms, Memory: 212884 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -2,25 +2,27 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-d = {0: 'D', 1: 'S', 2: 'L', 3:'R'}
+cmd_map = {0: 'D', 1: 'S', 2: 'L', 3: 'R'}
 
-def fun(x, i):
+def cal(x, i):
     if i == 0:
         return (x*2) % 10000
     elif i == 1:
         return (x-1) % 10000
     elif i == 2:
-        return x // 1000 + (x % 1000) * 10
+        # 1234 -> 2341
+        return (x % 1000) * 10 + x//1000
     else:
-        return x // 10 + (x % 10) * 1000
+        # 1234 -> 4123
+        return (x % 10) * 1000 + x//10
 
-    
 for _ in range(int(input())):
     a, b = map(int, input().split())
-
+    
     q = deque()
     q.append((a, ""))
-    visited = [False] * 10000
+    visited = [False] * 10_001
+    visited[a] = True
     
     while q:
         x, cmd = q.popleft()
@@ -30,8 +32,9 @@ for _ in range(int(input())):
             break
         
         for i in range(4):
-            nx = fun(x, i)
+            nx = cal(x, i)
             
             if not visited[nx]:
                 visited[nx] = True
-                q.append((nx, cmd+d[i]))
+                q.append((nx, cmd+cmd_map[i]))
+    
