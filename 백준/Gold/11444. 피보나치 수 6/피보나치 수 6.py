b7b437d7a9284109b23bcd4bb6174085acdd1861
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-07-03 10:05:26 +0900

    [Gold II] Title: 피보나치 수 6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+dp = dict()
+
+def sol(n):
+    if dp.get(n) != None:
+        return dp[n]
+    
+    if n == 0:
+        return 0
+    
+    if n == 1 or n == 2:
+        return 1
+    
+    dp[n//2+1] = sol(n//2+1) % 1_000_000_007
+    if n % 2 == 0:
+        dp[n//2-1] = sol(n//2-1) % 1_000_000_007
+        return (dp[n//2+1] ** 2 - dp[n//2-1] ** 2) % 1_000_000_007
+    else:
+        dp[n//2] = sol(n//2) % 1_000_000_007
+        return (dp[n//2+1] ** 2 + dp[n//2] ** 2) % 1_000_000_007
+
+print(sol(n))
