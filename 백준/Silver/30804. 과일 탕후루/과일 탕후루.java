936949b67c0a1377e0a849f3ec928aae80421a40
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-07-23 07:31:44 +0900

    [Silver II] Title: 과일 탕후루, Time: 316 ms, Memory: 33084 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -3,33 +3,32 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        int l = 0, r = 0, cnt = 0, kind = 0, ans = 0;
+        int l = 0, r = 0, kind = 0, cnt = 0, ans = 0;
         int[] nums = new int[10];
 
         while (r < n) {
-            if (nums[arr[r]] == 0)
-                kind ++;
-            cnt ++;
-            nums[arr[r]] ++;
+            if (++nums[arr[r]] == 1)
+                kind++;
+            cnt++;
 
-            if (kind > 2) {
+            if (2 < kind) {
                 if (--nums[arr[l]] == 0)
-                    kind --;
-                l++;
-                cnt--;
+                    kind--;
+                cnt --;
+                l ++;
             }
 
-            ans = Math.max(cnt, ans);
+            ans = Math.max(ans, cnt);
             r ++;
         }
 
         System.out.println(ans);
-
     }
 }
