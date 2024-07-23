54e478c74bb85f91ea854cb30ed7a896d4b2b824
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-07-24 08:27:34 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -5,32 +5,27 @@ arr = list(input().rstrip())
 stack = []
 ans = ''
 
-
 for x in arr:
     if x.isalpha():
         ans += x
-        continue
-    
-    if x == '(':
-        stack.append(x)
-        continue
-    
-    if x == '*' or x == '/':
-        while stack and (stack[-1] == '*' or stack[-1] == '/'):
-            ans += stack.pop()
-        stack.append(x)
-        continue
-    
-    if x == '+' or x == '-':
-        while stack and (stack[-1] != '('):
-            ans += stack.pop()
-        stack.append(x)
         continue
     
     if x == ')':
         while stack and (stack[-1] != '('):
             ans += stack.pop()
         stack.pop()
+        continue
+    
+    if x == '*' or x == '/':
+        while stack and (stack[-1] == '*' or stack[-1] == '/'):
+            ans += stack.pop()
+    
+    if x == '+' or x == '-':
+        while stack and (stack[-1] != '('):
+            ans += stack.pop()
+        
+    stack.append(x)
+
     
 while stack:
     ans += stack.pop()
