f7aeaef9e0011eeb0f6e078c47e70b4a0f5cac68
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-07-26 08:50:47 +0900

    [Gold IV] Title: DSLR, Time: 5436 ms, Memory: 302316 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,32 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Main {
-    static boolean[] visited;
 
+    static Map<Integer, String> cmdMap = Map.of(0, "D", 1, "S", 2, "L", 3, "R");
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        Map<Integer, String> d = Map.of(0, "D", 1, "S", 2, "L", 3, "R");
+        while (T -- > 0) {
+            String[] arr = br.readLine().split(" ");
+            int a = Integer.parseInt(arr[0]);
+            int b = Integer.parseInt(arr[1]);
 
-        for (int tc = 0; tc < t; tc++) {
-            String[] inputArr = br.readLine().split(" ");
-            int a = Integer.parseInt(inputArr[0]);
-            int b = Integer.parseInt(inputArr[1]);
-            
-            Queue<Register> q = new LinkedList<>();
-            q.add(new Register(a, ""));
-            visited = new boolean[10000];
+            Queue<Tuple> q = new LinkedList<>();
+            q.add(new Tuple(a, ""));
+            boolean[] visited = new boolean[10000];
             visited[a] = true;
 
             while (!q.isEmpty()) {
-                Register now = q.poll();
+                Tuple now = q.poll();
 
                 if (now.num == b) {
                     System.out.println(now.cmd);
@@ -34,35 +29,36 @@ class Main {
                 }
 
                 for (int i = 0; i < 4; i++) {
-                    int nextNum = fun(now.num, i);
-
-                    if (!visited[nextNum]) {
-                        visited[nextNum] = true;
-                        q.add(new Register(nextNum, now.cmd+d.get(i)));
+                    int newNum = process(i, now.num);
+                    if (! visited[newNum]) {
+                        visited[newNum] = true;
+                        q.add(new Tuple(newNum, now.cmd+cmdMap.get(i)));
                     }
                 }
             }
 
         }
+
     }
 
-    private static int fun(int x, int i) {
+    private static int process(int i, int x) {
         if (i == 0)
-            return (x*2) % 10000;
+            return (x * 2) % 10000;
         if (i == 1)
-            return x == 0 ? 9999 : (x-1) % 10000;
-        if (i == 2)
-            return x / 1000 + (x % 1000) * 10;
-        return x / 10 + (x % 10) * 1000;
+            return x == 0 ? 9999 : x - 1;
+        if (i == 2)     // 1234 -> 2341 
+            return (x % 1000) * 10 + x / 1000;
+        // 1234 -> 4123
+        return (x % 10) * 1000 + x / 10;
     }
+}
 
-    static class Register {
-        int num;
-        String cmd;
+class Tuple {
+    int num;
+    String cmd;
 
-        public Register(int num, String cmd) {
-            this.num = num;
-            this.cmd = cmd;
-        }
+    public Tuple(int num, String cmd) {
+        this.num = num;
+        this.cmd = cmd;
     }
 }
