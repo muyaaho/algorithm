b0b4b5b9680053b4fbf09f9aa394a71eaaf744ca
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-07-29 12:06:15 +0900

    [level 2] Title: 기능개발, Time: 3.12 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        ArrayList<Integer> tmp = new ArrayList<>();
+
+        int size = speeds.length;
+        Integer[] stack = new Integer[size];
+
+        int leftDay = 0, leftProgress = 0;
+        for (int i = 0; i < size; i++) {
+            leftProgress = 100 - progresses[i];
+            if (leftProgress % speeds[i] > 0) {
+                leftDay = (int)(leftProgress / speeds[i]) + 1;
+            }
+            else 
+                leftDay = (int)(leftProgress / speeds[i]);
+
+            stack[i] = leftDay;
+
+        }
+        // Arrays.sort(stack);
+        System.out.println(Arrays.toString(stack));
+        
+        int maxValue = 0;
+        for (int i = 0; i < size; i++) {
+            if (i == 0) {
+                tmp.add(stack[i]);
+                maxValue = stack[i];
+                continue;
+            }
+            
+            // System.out.println(maxValue+", "+ stack[i]);
+            if (maxValue < stack[i]) {
+                answer.add(tmp.size());
+                tmp.clear();
+                maxValue = stack[i];
+                tmp.add(stack[i]);
+                continue;
+            }
+            tmp.add(stack[i]);
+            maxValue = Math.max(maxValue, stack[i]);
+        }
+        
+        if (!tmp.isEmpty()) 
+            answer.add(tmp.size());
+        
+        // System.out.println(answer.toString());
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
