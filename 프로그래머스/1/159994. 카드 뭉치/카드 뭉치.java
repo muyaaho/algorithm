1f95ec4e11ce5dd15f6872d585a09d400641705f
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-08-01 20:10:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        
+        int c1Idx = 0, c2Idx = 0;
+        // System.out.println(goal[0].compareTo(cards1[c1Idx]));
+        for(String word : goal) {
+            if (c1Idx < cards1.length && word.compareTo(cards1[c1Idx]) == 0) {
+                c1Idx ++;
+                continue;
+            }
+            if (c2Idx < cards2.length && word.compareTo(cards2[c2Idx]) == 0) {
+                c2Idx ++;
+                continue;
+            }
+            answer = "No";
+            break;
+    
+        }
+        
+        return answer;
+    }
+}
