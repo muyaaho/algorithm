7799ac2c3e07372f18e7fcba84a6b934bf92d06b
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-08-05 00:00:34 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 784 ms, Memory: 195984 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -6,69 +6,21 @@ class Main {
     public static int n, m;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] cntArr = new int[20_000_001];
 
         n = Integer.parseInt(br.readLine());
-        arrN = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arrN[i] = Integer.parseInt(st.nextToken());
+            cntArr[Integer.parseInt(st.nextToken()) + 10_000_000] ++;
         }
 
         m = Integer.parseInt(br.readLine());
-        int[] arrM = new int[m];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            arrM[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arrN);
         StringBuilder sb = new StringBuilder();
-        for (int x : arrM) {
-            // System.out.println("x: "+x);
-            int start = 0, end=n-1;
-            int leftIndex = getIndex(x, true);
-            int rightIndex = getIndex(x, false);
-
-            if (rightIndex > -1 && leftIndex > -1)
-                sb.append((rightIndex - leftIndex+1)).append(" ");
-            else
-                sb.append(0).append(" ");
+        for (int i = 0; i < m; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            sb.append(cntArr[x+10_000_000]).append(" ");
         }
         System.out.println(sb);
-
-    }
-
-    private static int getIndex(int x, boolean isLeft) {
-        int start = 0;
-        int end = n-1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (x == arrN[mid]) {
-                if (isLeft) {
-                    if (mid > 0 && arrN[mid-1] == x) {
-                        end = mid-1;
-                        continue;
-                    }
-                    return mid;
-                }
-                else {
-                    if (x == arrN[mid]) {
-                        if (mid < n -1 && arrN[mid + 1] == x) {
-                            start = mid+1;
-                            continue;
-                        }
-                        return mid;
-                    }
-                }
-            }
-
-            else if (x < arrN[mid]) {
-                end = mid-1;
-            } else {
-                start = mid+1;
-            }
-        }
-        return -1;
     }
 }
