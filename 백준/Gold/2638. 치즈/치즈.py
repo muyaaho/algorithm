80a65c18c24cc59348c0ba6b5d47f689f0f3dcc5
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-08-06 08:39:23 +0900

    [Gold III] Title: 치즈, Time: 500 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -6,19 +6,20 @@ n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [0, 0, 1, -1]
-dy = [-1, 1, 0, 0]
+dy = [1, -1, 0, 0]
 
-def bfs():
+def dfs():
     q = deque()
     q.append((0, 0))
+    visited = [[0] * m for _ in range(n)]
     visited[0][0] = 1
     
     while q:
         x, y = q.popleft()
         
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = dx[i] + x
+            ny = dy[i] + y
             
             if 0 <= nx < n and 0 <= ny < m:
                 if visited[nx][ny] == 0 and arr[nx][ny] == 0:
@@ -26,18 +27,18 @@ def bfs():
                     visited[nx][ny] = 1
                 elif arr[nx][ny] == 1:
                     visited[nx][ny] += 1
+    
+    return visited
 
-
-air_cnt = 0
 ans = 0
-while air_cnt < n*m:
-    ans += 1
-    visited = [[0] * m for _ in range(n)]
-    bfs()
+air_cnt = 0
+
+while air_cnt < n * m:
+    visited = dfs()
     
     for i in range(n):
         for j in range(m):
-            if visited[i][j] >= 2:
+            if visited[i][j] > 1:
                 arr[i][j] = 0
     
     cnt = 0
@@ -45,5 +46,6 @@ while air_cnt < n*m:
         cnt += line.count(0)
     
     air_cnt = cnt
+    ans += 1
 
 print(ans)
