4bba753adcbdc3924a94a47994a7f00e7cf67509
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-08-07 08:02:04 +0900

    [Gold III] Title: 치즈, Time: 480 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -5,10 +5,10 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-def dfs():
+def bfs():
     q = deque()
     q.append((0, 0))
     visited = [[0] * m for _ in range(n)]
@@ -18,23 +18,24 @@ def dfs():
         x, y = q.popleft()
         
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
+            nx = x + dx[i]
+            ny = y + dy[i]
             
             if 0 <= nx < n and 0 <= ny < m:
                 if visited[nx][ny] == 0 and arr[nx][ny] == 0:
-                    q.append((nx, ny))
                     visited[nx][ny] = 1
+                    q.append((nx, ny))
                 elif arr[nx][ny] == 1:
                     visited[nx][ny] += 1
     
     return visited
 
-ans = 0
 air_cnt = 0
+answer = 0
 
-while air_cnt < n * m:
-    visited = dfs()
+while air_cnt < n*m:
+    answer += 1
+    visited = bfs()
     
     for i in range(n):
         for j in range(m):
@@ -44,8 +45,7 @@ while air_cnt < n * m:
     cnt = 0
     for line in arr:
         cnt += line.count(0)
-    
-    air_cnt = cnt
-    ans += 1
 
-print(ans)
+    air_cnt = cnt
+
+print(answer)
