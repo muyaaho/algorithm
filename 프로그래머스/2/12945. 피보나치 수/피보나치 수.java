c6c6ea7846710a17b320fd34d52f5b4981445945
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-08-11 22:43:37 +0900

    [level 2] Title: 피보나치 수, Time: 387.97 ms, Memory: 672 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,21 @@
+import java.math.BigInteger;
+
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        int a = 0, b = 1, tmp = 0;
-        
-        while (n-- > 0) {
-            tmp = b;
-            b = (a + b) % 1234567;
-            a = tmp;
+        public static BigInteger solution(int n) {
+
+        BigInteger[] array = new BigInteger[n+1];
+
+        array[0] = BigInteger.valueOf(0);
+        array[1] = BigInteger.valueOf(1);
+
+
+        for(int i=2;i<=n;i++){
+//            array[i-1].add(array[i-2]);
+            array[i] =   array[i-1].add(array[i-2]);
         }
-        return a % 1234567;
+
+//        System.out.println(Arrays.toString(array));
+        BigInteger a= array[n].remainder(BigInteger.valueOf(1234567));
+        return a;
     }
 }
