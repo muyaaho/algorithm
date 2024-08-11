ed9caed1ec7fa37852e088bc677f1deaa1f7bffe
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-08-11 11:57:44 +0900

    [level 3] Title: 정수 삼각형, Time: 9.47 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < i+1; j++) {
+                if (j == 0) {
+                    triangle[i][j] += triangle[i-1][j];
+                }
+                else if (j == i) {
+                    triangle[i][j] += triangle[i-1][j-1];
+                }
+                else {
+                    triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
+                }
+            }
+        }
+        int[] last = triangle[triangle.length-1];
+        for (int i : last) {
+            answer = Math.max(answer, i);
+        }
+        return answer;
+    }
+}
