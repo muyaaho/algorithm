53b8c555b8d3736f50348a75d18e69e110ac5a4b
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-08-14 10:18:22 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        while (storey > 0) {
+            int remainder = storey % 10;
+            
+            if (remainder > 5) {
+                answer += (10 - remainder);
+                storey += 10;
+            } else if (remainder < 5) {
+                answer += remainder;
+            }else {
+                if ((storey / 10) % 10 > 4) {
+                    storey += 10;
+                }
+                answer += remainder;
+            }
+            storey /= 10;
+        }
+        
+        return answer;
+    }
+}
