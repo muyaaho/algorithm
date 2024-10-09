4f6abcb9b609125c0173a3631b5a997076b3a922
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-10-09 22:22:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.91 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -6,55 +6,53 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         Set<Integer> set = new HashSet<>();
-        
+
         // graph 만들기
-        for(int[] x: wires) {
-            int a = x[0], b = x[1];
+        for(int[] nodes: wires) {
+            int a = nodes[0], b = nodes[1];
             set.add(a);
             set.add(b);
             graph.computeIfAbsent(a, key -> new ArrayList<>()).add(b);
             graph.computeIfAbsent(b, key -> new ArrayList<>()).add(a);
         }
-        
+
         // answer 업데이트
-        for (int[] x: wires) {
-            int cutA = x[0], cutB = x[1];
-            visited = new boolean[Collections.max(set) + 1];
+        for (int[] cut: wires) {
+            visited = new boolean[101];
             int[] tmp = new int[2];
             int tmpIdx = 0;
-            
+
             for (int node : set) {
                 if (!visited[node]) {
-                    tmp[tmpIdx++] = bfs(cutA, cutB, node);
+                    tmp[tmpIdx++] = bfs(cut[0], cut[1], node);
                 }
             }
             answer = Math.min(answer, Math.abs(tmp[0] - tmp[1]));
         }
         return answer;
     }
-    
+
     private int bfs(int cutA, int cutB, int start) {
         int count = 0;
         Deque<Integer> q = new ArrayDeque<>();
         q.offer(start);
         visited[start] = true;
-        
+
         while (!q.isEmpty()) {
             int now = q.poll();
             count++;
-            
-            List<Integer> nexts = graph.get(now);
-            for (int i = 0; i < nexts.size(); i++) {
-                int next = nexts.get(i);
-                
+
+            List<Integer> nextNodes = graph.get(now);
+            for (int i = 0; i < nextNodes.size(); i++) {
+                int next = nextNodes.get(i);
+
                 if (!(now == cutA && next == cutB || now == cutB && next == cutA) && !visited[next]) {
                     q.offer(next);
                     visited[next] = true;
                 }
-                
             }
         }
-        
+
         return count;
     }
 }
