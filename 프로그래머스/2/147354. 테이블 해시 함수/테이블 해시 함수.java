0e51f2db0d9d3e91c01d7585b29aff8a7e00eee9
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-10-16 10:51:47 +0900

    [level 2] Title: 테이블 해시 함수, Time: 13.57 ms, Memory: 142 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        
+        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> {
+            if (o1[col-1] != o2[col-1]) {
+                return o1[col-1] - o2[col-1];
+            }
+            return o2[0] - o1[0];
+        });
+        
+        for (int[] tuple : data) {
+            q.offer(tuple);
+        }
+        
+        int i = 0;
+        while (!q.isEmpty() && i < row_end){
+            int[] now = q.poll();
+            if (i >= row_begin - 1 && i < row_end) {
+
+                int si = 0;
+                for (int x : now) {
+                    si += (x % (i + 1));
+                }                
+                
+                if (i == row_begin - 1) answer = si;
+                else answer ^= si;
+            }
+            
+            i++;
+        }
+
+        return answer;
+    }
+}
