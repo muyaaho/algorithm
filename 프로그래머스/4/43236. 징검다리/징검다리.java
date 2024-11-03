62c6baa9a62ed5bbe5423f61b566085fbc86bd5c
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-11-04 00:38:01 +0900

    [level 4] Title: 징검다리, Time: 670.05 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        
+        Arrays.sort(rocks);
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(0);
+        for (int i = 0; i < rocks.length; i++) {
+            arr.add(rocks[i]);
+        }
+        if (rocks[rocks.length-1] != distance) {
+            arr.add(distance);
+        }
+        
+        
+        int min = 0, max = distance;
+        while (min <= max) {
+            int target = (min + max) / 2;
+            
+            
+            if (check(arr, n, arr.size(), target)) {
+                min = target+1;
+                answer = target;
+            }
+            else {
+                max = target - 1;
+                
+            }
+            
+        }
+        
+        return answer;
+    }
+    
+    private boolean check(ArrayList<Integer> arr, int n, int len,  int target) {
+        boolean[] deleted = new boolean[len];
+        int now = 0;
+        
+        // System.out.println("target: "+target);
+        
+        // 바위를 n개 빼는 과정 (deleted false로 만든다)
+        // 참고로 0과 마지막은 뺄 수 없어!!!
+        while (n > 0) {
+            
+            if (now + 1 >= len) {
+                now = 0;
+                break;
+            }
+            
+            int next = now+1;
+
+            while (deleted[next]) {
+                next ++;
+                if (next == len) {
+                    now = 0;
+                    next = now;
+                }
+            }
+            
+            
+            int dist = arr.get(next) - arr.get(now);
+
+            // 여기서 바위를 제거한다.
+            if (dist < target) {
+                n--;
+                // 마지막 값은 제거할 수 없어 -> now를 제거해(마지막만. 그래도 됨?) 그리도 태초마을 가
+                if (next == len-1) {
+                    deleted[now] = true;
+                    now = 0;
+                    continue;
+                }
+
+                deleted[next] = true;
+
+            } else {
+                // 바위를 제거하지 않으면 now 업데이트 하고 그냥 넘어가야지 뭐
+                now = next;
+            }
+        }
+        
+        // 바위를 제거하고도 작은거 남으면 false, 아니면 true
+        
+        // System.out.println("deleted: " + Arrays.toString(deleted));
+        // n이 끝나도 남은 거리가 있을 수 있음 -> now라는 건 그 앞은 조건을 만족했기 때문에 뒷부분만 확인하면 됨
+        // for문 써서 past를 now로 하고 false인 부분 체크하자 그리고 for문 그대로 끝나버리면 true 리턴하고
+        
+        for (int next = now + 1; next < len; next++) {
+            if (!deleted[next]) {
+                if (arr.get(next) - arr.get(now) < target) {
+                    return false;
+                }
+                now = next;
+            }
+        }
+           
+        // 이 모든 정보가 걸리지 않으면
+        return true;
+    }
+    
+    
+}
