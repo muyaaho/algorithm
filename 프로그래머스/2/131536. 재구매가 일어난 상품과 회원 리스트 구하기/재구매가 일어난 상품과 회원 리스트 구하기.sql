fd680aa6c3a3765481340f588a9100c2cf76231a
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-11-12 01:01:39 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,46 +1,5 @@
-# -- 코드를 입력하세요
-# SELECT USER_ID, PRODUCT_ID
-# from online_sale
-# where user_id in (
-#     select user_id
-#     from online_sale
-#     group by user_id
-#     having count(*) > 1
-#     ) and 
-#     sales_date > (
-#     select min(sales_date)
-#     from online_sale
-#     group by user_id
-#     )
-# order by user_id asc, product_id desc
-
-# SELECT *
-# SELECT USER_ID, PRODUCT_ID
-# FROM ONLINE_SALE
-# WHERE USER_ID IN ( SELECT USER_ID
-#                    FROM ONLINE_SALE
-#                    GROUP BY USER_ID
-#                    HAVING COUNT(*) > 1 AND MIN(SALES_DATE) < SALES_DATE)
-                
-# ORDER BY USER_ID ASC, PRODUCT_ID DESC;
-
 SELECT USER_ID, PRODUCT_ID
 FROM ONLINE_SALE
 GROUP BY USER_ID, PRODUCT_ID
 HAVING COUNT(USER_ID) > 1
 ORDER BY USER_ID, PRODUCT_ID DESC;
-
-
-
-# SELECT USER_ID, PRODUCT_ID
-# FROM ONLINE_SALE
-# WHERE SALES_DATE IN (SELECT SALES_DATE
-#                    FROM ONLINE_SALE
-#                    GROUP BY USER_ID
-#                     HAVING )
-# ORDER BY USER_ID ASC, PRODUCT_ID DESC;
-
-
-# select *
-# from online_sale
-# order by user_id, sales_date
